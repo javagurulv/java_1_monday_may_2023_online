9ca71c5f349e928a5d7e8650b6b63c7b854775da
javagurulv <javagurulv@gmail.com>
2023-06-05 18:00:58 +0300

    project 4

---
+++ src/main/java/lv/javaguru/java1/teacher/project_4/practice/HelloWorld.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.teacher.project_4.practice;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+
+}
