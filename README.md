ca53e47ff8aa24926d33d12d36b6b3f73f877625
javagurulv <javagurulv@gmail.com>
2023-06-05 16:14:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![build](https://github.com/javagurulv/java_1_monday_may_2023_online/actions/workflows/build.yaml/badge.svg)
---
+++ src/main/java/lv/javaguru/java1/teacher/Test.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.teacher;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
