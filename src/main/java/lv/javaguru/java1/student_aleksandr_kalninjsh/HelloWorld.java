92eddd28ae4b5a28a90c8e3925323e2db0668570
Asus <tehnokontrolj@inbox.lv>
2023-06-08 16:45:37 +0300

    Created HelloWorld

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/HelloWorld.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from java!");
+        System.out.println("Hello!");
+        System.out.println("Hello java!");
+
+        int number1 = 12232433;
+        int number2 = 4554654;
+        int result = number1 + number2;
+        System.out.println("Result: = " + result);
+    }
+}
