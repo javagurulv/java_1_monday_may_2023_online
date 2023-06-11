fb728b50ba17957ae7f23bde3b224eb9849300e4
Asus <tehnokontrolj@inbox.lv>
2023-06-11 23:58:32 +0300

    Lesson_1_level_4 Task_10;Task_11

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_1_level_4/Task_10.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_1_level_4;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+
+        System.out.println(74 + 36);
+        System.out.println(50 / 3);
+
+    }
+
+}
