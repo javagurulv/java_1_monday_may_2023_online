f8747e7e7c5da95fc4db72083f3aa2b65205cd34
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-12 20:40:52 +0100

    int long sum example

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/TwoIntSumApp.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+public class TwoIntSumApp {
+
+
+    public static void main(String[] args) {
+        // -2 (32) до 2 (32)
+        int number1 = 85894589;
+        int number2 = 4366447;
+        int result = number1 + number2;
+        System.out.println("Sum result = " + result);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/TwoLongSumApp.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+public class TwoLongSumApp {
+
+
+    public static void main(String[] args) {
+        // -2 (64) до 2 (64)
+        long number1 = 85894589L;
+        long number2 = 4366447L;
+        long result = number1 + number2;
+        System.out.println("Sum result = " + result);
+
+    }
+}
