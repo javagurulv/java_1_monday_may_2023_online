24423431ad683d54d0d3dd092ffd72fe61817f3d
Asus <tehnokontrolj@inbox.lv>
2023-06-12 20:39:26 +0300

    int sum

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/TwoIntSumApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2;
+
+public class TwoIntSumApp {
+
+    public static void main(String[] args) {
+
+        int number1 = 100;
+        int number2 = 200;
+        int result = number1 + number2;
+
+        System.out.println("Sum result = " + result);
+    }
+}
