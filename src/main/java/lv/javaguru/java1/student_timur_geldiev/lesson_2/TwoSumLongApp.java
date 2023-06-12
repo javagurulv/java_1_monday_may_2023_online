d1c061d934207eb75bf9eaa52e04dc97dd713b3b
GTimur <timur.geldiev@28stone.com>
2023-06-12 20:40:38 +0300

    Lesson2Task5 int and long variables example

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_2/TwoSumLongApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_2;
+
+public class TwoSumLongApp {
+
+    public static void main(String[] args) {
+
+        long number1 = 999999999999999999L;
+        long number2 = 999999999999999999L;
+        long result = number1 + number2;
+        System.out.println("Result is " + result);
+
+    }
+}
