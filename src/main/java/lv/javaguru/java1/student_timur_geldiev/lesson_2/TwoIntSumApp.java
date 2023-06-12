b2d563ff7bc5dd94ca955a6e80ed5b80bc0dd882
GTimur <timur.geldiev@28stone.com>
2023-06-12 20:30:50 +0300

    Lesson2Task5

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_2/TwoIntSumApp.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_2;
+
+public class TwoIntSumApp {
+
+    public static void main(String[] args){
+
+        int number1 = 100;
+        int number2 = 200;
+        int result = number1 + number2;
+        int average = result / 2;
+        System.out.println("Result is " + result);
+        System.out.println("Average number is " + average);
+    }
+}
