7ddfeeaa80109a6fad1ed00ab9b2a7f33950e937
javagurulv <javagurulv@gmail.com>
2023-06-12 17:41:49 +0300

    lessons 3 homeworks

--- src/main/java/lv/javaguru/java1/teacher/lesson_1_introduction/lessoncode/MyCar.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.java1.teacher.lesson_1_introduction.lessoncode;
-
-class MyCar {
-
-    public static void main(String[] args) {
-        System.out.println("MyCar!");
-    }
-
-}
