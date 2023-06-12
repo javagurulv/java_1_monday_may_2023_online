4095d00b993bc246f891194c48a744fc230471a5
Asus <tehnokontrolj@inbox.lv>
2023-06-12 20:45:24 +0300

    long sum

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/TwoLongSumApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2;
+
+public class TwoLongSumApp {
+
+    public static void main(String[] args) {
+
+        long number1 = 100888888777776665L;
+        long number2 = 2009999696969858547L;
+        long result = number1 + number2;
+
+        System.out.println("Sum result = " + result);
+    }
+}
