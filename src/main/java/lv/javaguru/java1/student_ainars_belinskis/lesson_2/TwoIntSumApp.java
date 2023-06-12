39c581abbfa2582b630aefdf32814305b8d5653c
AinarsB <adad@gmail.com>
2023-06-12 20:39:13 +0300

    Lesson_2 int and long numbers summ

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/TwoIntSumApp.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2;
+
+public class TwoIntSumApp {
+
+    public static void main(String[] args) {
+        int number1 = 222;
+        int number2 = 333;
+
+        int result = number1 + number2;
+
+        System.out.println("Sum result = " + result);
+    }
+
+
+
+}
