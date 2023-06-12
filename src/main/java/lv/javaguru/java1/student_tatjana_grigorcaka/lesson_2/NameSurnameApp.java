28162fb0bbab19cfb3b19f2107ecd20d06524d28
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-12 19:56:25 +0100

    Lesson_2 task1 and task2

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/NameSurnameApp.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+public class NameSurnameApp {
+
+    public static void main(String[] args) {
+        System.out.println("Tatjana");
+        System.out.println("Grigorcaka");
+    }
+}
+
+
