a5a93e0ea6d6655674f7fe09699a11adf963705a
GTimur <timur.geldiev@28stone.com>
2023-06-12 19:27:49 +0300

    Lesson2Task1/2

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/HelloWorld.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_timur_geldiev;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_2/NameSurnameApp.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_2;
+
+public class NameSurnameApp {
+    public static void main(String[] args) {
+        System.out.println("Timur");
+        System.out.println("Geldiev");
+    }
+}
