58948dc744a980fd5f71a9ef442ac08f2c8322a7
AinarsB <adad@gmail.com>
2023-06-12 21:16:59 +0300

    Lesson_2 int and long numbers summ

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/ConcatenateTextApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2;
+
+public class ConcatenateTextApp {
+
+    public static void main(String[] args) {
+       String firstName = "Ainars";
+        String lastName = "Belinskis";
+        String fulltName = firstName + " " + lastName;
+        System.out.println(fulltName);
+    }
+
+
+}
