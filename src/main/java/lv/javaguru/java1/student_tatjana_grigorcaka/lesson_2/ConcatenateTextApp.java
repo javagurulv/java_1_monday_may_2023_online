e0a8cde9aedf86dd9d5676431f45255df275b708
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-12 21:18:55 +0100

    concatenate strings example

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/ConcatenateTextApp.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+public class ConcatenateTextApp {
+
+
+    public static void main(String[] args) {
+        String firstName = "Vasja";
+        String lastName = "Pupkin";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+
+
+
+
+
+
+
+}
