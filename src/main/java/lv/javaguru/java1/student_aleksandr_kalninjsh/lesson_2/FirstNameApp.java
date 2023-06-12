7a17e885ecd1ff1425f0b3dd7f596fd33285f0aa
Asus <tehnokontrolj@inbox.lv>
2023-06-12 19:54:27 +0300

    Lesson_2 Task1 Task2

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/FirstNameApp.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2;
+
+public class FirstNameApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Aleksandr");
+    }
+}
