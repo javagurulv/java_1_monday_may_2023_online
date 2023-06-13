f0e140db8c1426328c3308fff1ccbac35b8964af
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-14 00:54:22 +0100

    triangle square

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/TriangleSquare.java
@@ -0,0 +1,29 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+import java.util.Scanner;
+
+public class TriangleSquare {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a: ");
+        int aSide = in.nextInt();
+
+        System.out.print("Input b: ");
+        int bSide = in.nextInt();
+
+        System.out.print("Input c: ");
+        int cSide = in.nextInt();
+
+        System.out.println("A side = " + aSide);
+        System.out.println("B side = " + bSide);
+        System.out.println("C side = " + cSide);
+
+        double p = (aSide + bSide + cSide) / 2;
+        double square = Math.sqrt(p * (p - aSide) * (p - bSide) * (p - cSide));
+
+        System.out.println("Triangle square = " + square);
+    }
+
+}
