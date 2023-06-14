d410839fb1d7e0915f6c955d94e6f25de5bdf0f9
AinarsB <abelinskis@gmail.com>
2023-06-14 19:21:05 +0300

    Lesson_2 Homework Day_2

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/homework/day_2/HelloName.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.homework.day_2;
+
+import java.util.Scanner;
+public class HelloName {
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.print("What's your name?: ");
+            String name = in.nextLine();
+
+            System.out.println("Hello " + name +"!");
+
+        }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/homework/day_2/LoginService.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.homework.day_2;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+}
