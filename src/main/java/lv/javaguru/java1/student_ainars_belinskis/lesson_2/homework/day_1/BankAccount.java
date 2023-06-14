4dac747e413eacdfac6bbcf90ec59cda5410c894
AinarsB <abelinskis@gmail.com>
2023-06-14 18:35:13 +0300

    Lesson_2 Homework Day1

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/homework/day_1/BankAccount.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.homework.day_1;
+
+public class BankAccount {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/homework/day_1/Phone.java
@@ -0,0 +1,4 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.homework.day_1;
+
+public class Phone {
+}
