88fe83a87a1090f7e9e31dd7cb405ec3b3b26c98
javagurulv <javagurulv@gmail.com>
2023-06-14 14:30:06 +0300

    lesson 4

--- src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_2/homework/weekly_challenges/task5/OneLineAndMultiLineComment.java
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_2/homework/weekly_challenges/task5/OneLineAndMultiLineComment.java
@@ -3,7 +3,7 @@ package lv.javaguru.java1.student_timur_geldiev.lesson_2.homework.weekly_challen
 public class OneLineAndMultiLineComment {
 
     // This line is written to show an example of one line comment
-    So, on the next line this is not a comment anymore
+    // Fix we need project to compile :) : So, on the next line this is not a comment anymore
 
     /* This is an example of multiline comment
     So, on the next line comment continue
