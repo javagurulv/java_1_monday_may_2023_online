045ea83a91c4efe482fe9a1772909b351e8f783e
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-15 00:55:30 +0100

    time app

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/TimeApp.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2;
+
+import java.util.Scanner;
+
+public class TimeApp {
+
+        public static void main(String[] args)   {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input Time in seconds: ");
+        int inputTime = in.nextInt();
+
+        int hour = inputTime / 3600;
+        int min = (inputTime % 3600) / 60;
+        int sec = (inputTime % 3600) % 60;
+
+        System.out.println( hour + ":" + min + ":" + sec);
+    }
+
+}
+
