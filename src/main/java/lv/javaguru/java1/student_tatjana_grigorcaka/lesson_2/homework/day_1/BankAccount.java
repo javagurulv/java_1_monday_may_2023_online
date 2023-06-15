88c65b7fa0bd98e56e96ee9ec9fa9c3a856d9c8d
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-16 00:25:10 +0100

    lesson 2 homework day 1 bank account and turtle

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/homework/day_1/BankAccount.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.homework.day_1;
+
+public class BankAccount {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/homework/day_1/Turtle.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.homework.day_1;
+
+public class Turtle {
+
+    public static void main(String[] args) {
+
+    }
+}
