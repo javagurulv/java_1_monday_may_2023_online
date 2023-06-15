840fe7921378ecc892786b2df88ef3abaeda08d7
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-16 00:27:56 +0100

    lesson 2 homework day 2 login service and hello name

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/homework/day_2/HelloUserName.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.homework.day_2;
+
+import java.util.Scanner;
+public class HelloUserName {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your name:");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+
+        System.out.println("Hello " + "Tatjana!");
+
+    }
+
+}
