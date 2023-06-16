426ff8b8ad7b87641cd0c8308816d1e4b2b6b84a
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-16 22:50:35 +0100

    lesson 2 homework day 3 multiplication table

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/homework/day_3/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.homework.day_3;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int number = in.nextInt();
+
+        for (int i=1; i< 10; i++) {
+
+            System.out.println (number + " * "+ i +" = " + number * i);;
+        }
+    }
+}
