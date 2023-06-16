36b069db9a32b79220256d9edb0dfd39ff1bfd1e
westor0007 <dfglkj@ya.ru>
2023-06-16 21:41:38 +0300

    test

---
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_2/classwork/FirstNameApp.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_butrin_ilya.lesson_2.classwork;
+
+public class FirstNameApp {
+    public static void main(String[] args) {
+
+        System.out.println("Ilya");
+    }
+}
+
