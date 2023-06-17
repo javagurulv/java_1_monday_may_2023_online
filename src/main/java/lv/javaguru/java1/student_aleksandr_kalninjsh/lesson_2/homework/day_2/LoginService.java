3500e85b2e1842a9a84dc1455c88d3cd3a7a8cbb
Asus <tehnokontrolj@inbox.lv>
2023-06-17 16:14:01 +0300

    LoginService; Task_2

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/homework/day_2/LoginService.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2.homework.day_2;
+
+public class LoginService {
+
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
