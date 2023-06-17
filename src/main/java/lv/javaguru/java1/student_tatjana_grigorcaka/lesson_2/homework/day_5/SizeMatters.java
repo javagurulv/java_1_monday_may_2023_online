a1149b8a9dc94ab3828ae0e088de3b2978b3d5a2
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-17 18:54:02 +0100

    lesson 2 homework day 5 size matters

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/homework/day_5/SizeMatters.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.homework.day_5;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+
+}
