ad27e0d625f0706454e55aaeb75ca07be87097ff
AinarsB <abelinskis@gmail.com>
2023-06-18 01:41:57 +0300

    Lesson_2 Task13 Task14

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/lessoncode/CheckEvenOdd.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.lessoncode;
+
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+       public static void main(String args[]) {
+               System.out.println("Введите число: ");
+
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+        if ( num % 2 == 0 )
+            System.out.println("Число чётное");
+        else
+            System.out.println("Число нечётное");
+    }
+}
+
+
---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_2/lessoncode/Temperatura.java
@@ -0,0 +1,23 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_2.lessoncode;
+
+import java.util.Scanner;
+
+public class Temperatura {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите температуру в градусах Цельсия: ");
+        float celsia = in.nextFloat();
+
+        double farengeit = celsia * 1.8 + 32;
+        double kelvin = celsia + 273.15;
+
+        System.out.println("температуру в градусах Цельсия: " + celsia);
+        System.out.println("температуру в градусах Фаренгейта: " + farengeit);
+        System.out.println("температуру в градусах Кельвина: " + kelvin);
+
+    }
+
+}
