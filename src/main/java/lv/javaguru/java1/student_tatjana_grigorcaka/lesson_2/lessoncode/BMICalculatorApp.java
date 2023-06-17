ac59de7608e3f49363fd2283c95c8fdc9dfe55e2
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-18 00:21:36 +0100

    lesson 2 lessoncode bmi calculator

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/lessoncode/BMICalculatorApp.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.lessoncode;
+
+import java.util.Scanner;
+public class BMICalculatorApp {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input weight in kilogram: ");
+        double weight = sc.nextDouble();
+        System.out.print("Input height in meters: ");
+        double height = sc.nextDouble();
+
+        double BMI = weight / (height * height);
+
+        System.out.print("The Body Mass Index (BMI) is " + BMI + " kg/m2");
+    }
+
+    }
