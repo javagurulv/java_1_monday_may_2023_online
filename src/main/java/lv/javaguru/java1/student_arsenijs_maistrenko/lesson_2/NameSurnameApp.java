cdcdbeb092d70b55b74ce5ead4b25e565fad22bf
Auraiel <st78564@students.tsi.lv>
2023-06-17 15:17:07 +0300

    lesson_2 taks1+2

---
+++ src/main/java/lv/javaguru/java1/student_arsenijs_maistrenko/lesson_2/NameSurnameApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_arsenijs_maistrenko.lesson_2;
+
+public class NameSurnameApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Arseny");
+        System.out.println("Maistrenko");
+    }
+}
