0d61663829d3301c9f06444643a030c25f5b370e
Auraiel <st78564@students.tsi.lv>
2023-06-17 15:29:44 +0300

    lesson_2 taks5

---
+++ src/main/java/lv/javaguru/java1/student_arsenijs_maistrenko/lesson_2/TwoIntSumApp.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_arsenijs_maistrenko.lesson_2;
+
+public class TwoIntSumApp {
+    public static void main(String[] args) {
+
+        int number1 = 10054;
+        int number2 = 28200;
+        int result = number1 + number2;
+        System.out.println("Sum: " + result);
+        System.out.println(result);
+    }
+}
