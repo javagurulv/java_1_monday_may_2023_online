132498e4096d5d88596941f84d31b858a2c74c3e
Alex-Likurg <likurg68@gmail.com>
2023-06-18 22:33:19 +0300

    Long and Float numbers

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/workinclass/SummaChiselFloat.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.workinclass;
+
+public class SummaChiselFloat {
+    public static void main(String[]args) {
+        //float - маленькие вещественные числа
+        //double - большие вещественные числа
+
+        float number1 = 1.5F;
+        float number2 = 3.2F;
+        float result = number1 + number2;
+
+        float sumResult = number1 + number2;
+        float reduceResult = number1 - number2;
+        float multiplyResult = number1 * number2;
+        float divisionResult = number1 / number2;
+
+        System.out.println("Sum result = " + result);
+        System.out.println(result);
+
+    }
+}
