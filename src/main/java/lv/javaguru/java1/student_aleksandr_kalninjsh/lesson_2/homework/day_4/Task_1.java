f73b51bff3eac9517d7c69b64bdd0f392069647e
Asus <tehnokontrolj@inbox.lv>
2023-06-18 15:08:17 +0300

    Task_1

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/homework/day_4/Task_1.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2.homework.day_4;
+
+public class Task_1 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Text1 ");
+        System.out.print("Text2 ");
+
+    }
+}
