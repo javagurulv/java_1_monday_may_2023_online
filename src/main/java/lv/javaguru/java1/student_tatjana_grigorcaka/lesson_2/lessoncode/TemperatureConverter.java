9732c96b302c69f31873be6f74fc777b4ed505ef
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-18 16:32:53 +0100

    lesson 2 lessoncode temperature converter

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/lessoncode/TemperatureConverter.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.lessoncode;
+
+import java.util.Scanner;
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter temperature value in Celsius: ");
+        double celsius = input.nextDouble() ;
+
+        double fahrenheit = (celsius * 9/5) + 32;
+        double kelvin = celsius + 273.15;
+
+        System.out.println("Celsius: " + celsius + " C");
+        System.out.println("Fahrenheit: " + fahrenheit + " F");
+        System.out.println("Kelvin: " + kelvin + " K");
+    }
+
+}
+
