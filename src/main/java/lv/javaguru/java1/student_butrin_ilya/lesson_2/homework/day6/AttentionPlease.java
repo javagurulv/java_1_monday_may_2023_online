63da906f016581250fbe0dd85efd7a147d6583f7
westor0007 <dfglkj@ya.ru>
2023-06-18 14:42:41 +0300

    day 6,7

---
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_2/homework/day6/AttentionPlease.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_butrin_ilya.lesson_2.homework.day6;
+
+public class AttentionPlease {
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
+
+
---
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_2/homework/day7/NoSleepNoFocus.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_butrin_ilya.lesson_2.homework.day7;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+
+}
