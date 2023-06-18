e2a1ad501637fa44034100aad9fd916214be0ec5
Alex-Likurg <likurg68@gmail.com>
2023-06-18 22:52:48 +0300

    String Name + Surname

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/workinclass/NameSurnameTextApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.workinclass;
+
+public class NameSurnameTextApp {
+    public static void main(String[]args) {
+        String firstname = "Alexey";
+        String lastname = "Kosmachev";
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(fullname);
+
+
+    }
+}
