8596a5c1db98e622a0786739698dcaae03150da6
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-19 00:33:09 +0100

    lesson 2 lessoncode check even or odd

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_2/lessoncode/CheckEvenOdd.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_2.lessoncode;
+
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter number: ");
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        if ( num % 2 == 0 )
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");}
+
+}
