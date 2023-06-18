9ca36d0f2931d75ae3c8a3e117df7914d7061cf8
Alex-Likurg <likurg68@gmail.com>
2023-06-18 22:02:24 +0300

    int long summ example

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/LongSummaChisel.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2;
+
+public class LongSummaChisel {
+
+    // -2(64) do 2(64)
+    public static void main(String[]args) {
+        long number1 = 12345678909087L;
+        long number2 = 89888899945666L;
+        long result = number1 + number2;
+
+        System.out.println("Sum result = " + result);
+        System.out.println(result);
+
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/workinclass/SummaChisel.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.workinclass;
+
+public class SummaChisel {
+    public static void main(String[]args) {
+        int number1 = 100;
+        int number2 = 200;
+        int result = number1 + number2;
+
+        System.out.println("Sum result = " + result);
+        System.out.println(result);
+
+    }
+}
