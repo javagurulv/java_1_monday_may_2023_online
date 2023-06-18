ff65bd1af35d9fe1fddeabee9dbc04bb7e872a9f
Alex-Likurg <likurg68@gmail.com>
2023-06-18 21:28:46 +0300

    lesson_2 first two tasks My name and Surname

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/HelloWorld.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_alexey_kosmachev;
+
+public class HelloWorld {
+public static void main(String[]args) {
+    System.out.println("Hello from Java!");
+    System.out.println("Hello!");
+    System.out.println("Hello Java!");
+}
+
+
+}
