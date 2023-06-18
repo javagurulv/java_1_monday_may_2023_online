7bfe805a24ceb3e2933f79cf5f591e45bfb34d5c
Asus <tehnokontrolj@inbox.lv>
2023-06-18 15:36:22 +0300

    AttentionPlease

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_2/homework/day_6/AttentionPlease.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_2.homework.day_6;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not compile!");
+    }
+}
