4f9ca1503ae5598fbaad0af1a0146318d721a51a
Alex-Likurg <likurg68@gmail.com>
2023-06-18 21:20:57 +0300

    lesson_2 first two tasks My name and Surname

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/NameSurnameApp.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2;
+
+public class NameSurnameApp {
+    public static void main(String[]args) {
+        System.out.println("Alexey!");
+        System.out.println("Kosmachev!");
+    }
+}
