7804375532f61cd414a225d3e2886c977c386c6c
Vitalijs <vashisopli@gmail.com>
2023-06-19 17:45:00 +0300

    Lesson_2

---
+++ src/main/java/lv/javaguru/java1/student_vitalijs_laksa/test_apps/AreaTriangleDemo.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_vitalijs_laksa.test_apps;
+import java.util.Scanner;
+
+public class AreaTriangleDemo {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите ширину треугольника: ");
+        double base = scanner.nextDouble();
+
+        System.out.println("Введите высоту треугольника: ");
+        double height = scanner.nextDouble();
+
+        //Площадь треугольника вычисляется по формуле = (width*height)/2
+        double area = (base* height)/2;
+        System.out.println("Площадь треугольника равна: " + area);
+    }
+}
