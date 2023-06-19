07da7c773b2b57bb56e855fb123a2c10d4d94f3f
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2023-06-19 22:05:46 +0300

    lesson 3 task 13

--- src/main/java/lv/javaguru/java1/teacher/lesson_3_oop_first_look/lessoncode/QuestionnaireApp.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_3_oop_first_look/lessoncode/QuestionnaireApp.java
@@ -1,5 +1,7 @@
 package lv.javaguru.java1.teacher.lesson_3_oop_first_look.lessoncode;
 
+import java.util.Scanner;
+
 public class QuestionnaireApp {
 
     public static void main(String[] args) {
@@ -7,10 +9,16 @@ public class QuestionnaireApp {
         Question questionAboutJavaGuru = new Question("All fine?");
         Questionnaire questionnaire = new Questionnaire("Alex", questionAboutYourself, questionAboutJavaGuru);
 
+        questionnaire.questionOne.answer = true;
         System.out.println(questionnaire.questionOne.questionText);
         System.out.println(questionnaire.questionOne.answer);
 
+        questionnaire.questionTwo.answer = true;
         System.out.println(questionnaire.questionTwo.questionText);
         System.out.println(questionnaire.questionTwo.answer);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean nextBoolean = scanner.nextBoolean();
+        System.out.println(nextBoolean);
     }
 }
