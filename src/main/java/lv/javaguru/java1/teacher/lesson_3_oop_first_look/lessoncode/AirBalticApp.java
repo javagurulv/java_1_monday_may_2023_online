731df22511f9504fc7ab71fef44c9c483351d502
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2023-06-19 21:22:31 +0300

    lesson 3 task 11

--- src/main/java/lv/javaguru/java1/teacher/lesson_3_oop_first_look/lessoncode/AirBalticApp.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_3_oop_first_look/lessoncode/AirBalticApp.java
@@ -1,11 +1,13 @@
 package lv.javaguru.java1.teacher.lesson_3_oop_first_look.lessoncode;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class AirBalticApp {
 
     public static void main(String[] args) {
-        FlightTicket testTicket = new FlightTicket("RIX", "ABC", new Date(), new Date());
+        GregorianCalendar departureDateTime = new GregorianCalendar(2023, 05, 19, 23, 59);
+        FlightTicket testTicket = new FlightTicket("RIX", "ABC", departureDateTime.getGregorianChange(), new Date());
 
         System.out.println(testTicket.departureDateTime);
         System.out.println(testTicket.arrivalDateTime);
