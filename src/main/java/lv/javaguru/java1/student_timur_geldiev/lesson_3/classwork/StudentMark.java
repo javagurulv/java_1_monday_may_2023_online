f735d0f9f057f351c03c9dd7bd287a58b68088a0
TG_B <timur.geldiev@gmail.com>
2023-06-19 21:05:31 +0300

    Lesson3 Task10

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_3/classwork/StudentMark.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_3.classwork;
+
+public class StudentMark {
+
+    String studentName;
+    String studentLastName;
+    String subject;
+    int mark;
+}
