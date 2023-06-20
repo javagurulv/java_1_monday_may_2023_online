ac360f71f231e15634ddad78a380237de43cdf9f
AinarsB <abelinskis@gmail.com>
2023-06-21 01:18:51 +0300

    Lesson_3 Task14 Family FamilApp Person

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_3/lessoncode/Family.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_3.lessoncode;
+
+public class Family {
+    String familyNumber;
+    Person father;
+    Person mother;
+    Person childeOne;
+    Person childeTwo;
+
+    public Family (String familyNumber, Person father, Person mother, Person childeOne, Person childeTwo) {
+    this.familyNumber = familyNumber;
+    this.father = father;
+    this.mother = mother;
+    this.childeOne = childeOne;
+    this.childeTwo = childeTwo;
+
+    }
+}
