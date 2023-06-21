430f0af33d481a7492c1d8c0b9531424b1b57b9f
Asus <tehnokontrolj@inbox.lv>
2023-06-21 20:31:08 +0300

    Robot;RobotApp

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_3/day_1/Robot.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_3.day_1;
+
+public class Robot {
+
+    String name;
+
+    Robot(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
