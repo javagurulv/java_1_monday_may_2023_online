a674aa07e05a3aca78e1a7d18b6e7115743b4069
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-21 22:03:29 +0100

    lesson 3 lessoncode flight ticket,  airbaltic app

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/AirBalticApp.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+
+import java.util.Date;
+
+public class AirBalticApp {
+    public static void main(String[] args) {
+        FlightTicket testTicket = new FlightTicket("RIX", "ABC", new Date(), new Date());
+
+                System.out.println(testTicket.departureDateTime);
+                System.out.println(testTicket.arrivalDateTime);
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/FlightTicket.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+import java.util.Date;
+
+public class FlightTicket {
+
+    String departure;
+    String destination;
+    public Date departureDateTime;
+    public Date arrivalDateTime;
+
+    public FlightTicket(String departure, String destination, Date departureDateTime, Date arrivalDateTime) {
+        this.departure = departure;
+        this.destination = destination;
+        this.departureDateTime = departureDateTime;
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
+}
