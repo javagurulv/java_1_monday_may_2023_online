9d1a7bc31b2a133517f835a0855bcfbef6c18ca2
AinarsB <abelinskis@gmail.com>
2023-06-21 23:43:13 +0300

    Lesson_3 Homework Day_1

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_3/homework/day_1/Robot.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_3.homework.day_1;
+
+public class Robot {
+    String robotName;
+
+    public Robot(String robotName) {
+        this.robotName = robotName;
+    }
+
+    public String getRobotName() {
+        return robotName;
+    }
+
+    public void setRobotName(String robotName) {
+        this.robotName = robotName;
+
+    }
+}
