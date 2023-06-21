5e290e9f1754b95fee89dbb96ed24d34715b4679
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-22 00:56:13 +0100

    lesson 3 lessoncode questionnaire

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/Question.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+public class Question {
+
+    public String questionText;
+    public boolean answer;
+
+
+    //constructor
+
+    public Question(String questionText) {
+        this.questionText = questionText;
+    }
+
+    }
+
---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/Questionnaire.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+import lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode.Question;
+
+public class Questionnaire {
+    String name;
+    Question questionOne;
+    Question questionTwo;
+
+//constructor
+
+public Questionnaire(String name, Question questionOne, Question questionTwo)  {
+    this.name = name;
+    this.questionOne = questionOne;
+    this.questionTwo = questionTwo;
+
+}
+
+
+}
