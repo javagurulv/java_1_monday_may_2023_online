4514a2e04295775d0566ac4a4c6b91dcf10049df
westor0007 <dfglkj@ya.ru>
2023-06-22 23:18:02 +0300

    homework day 1

---
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_3/homework/day1/Robot.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_butrin_ilya.lesson_3.homework.day1;
+
+public class Robot {
+    String robotName;
+
+    public Robot (String x) {
+       robotName = x;
+
+    }
+public String getName () {
+        return robotName;
+}
+public void setName (String x) {
+        robotName = x;
+
+}
+}
---
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_3/homework/day1/RobotApp.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_butrin_ilya.lesson_3.homework.day1;
+
+public class RobotApp {
+
+    public static void main(String[] args) {
+        Robot rob1 = new Robot("Mers");
+        Robot rob2 = new Robot("Mers2");
+       // String robName1 = rob1.getName();
+       // String robName2 = rob2.getName();
+
+System.out.println(rob1.robotName);
+rob1.setName("NEW_Mers");
+System.out.println(rob2.robotName);
+rob2.setName("NEW_Mers2");
+        System.out.println(rob1.robotName);
+        System.out.println(rob2.robotName);
+
+    }
+}
