c579a395e2bf0cd475b50722d04892ac5b1f8ac6
AinarsB <abelinskis@gmail.com>
2023-06-22 22:42:48 +0300

    Lesson_3 Homework Day_3

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_3/homework/day_3/Chapter.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_3.homework.day_3;
+
+public class Chapter {
+    String chapterName;
+    String chapterText;
+
+    public Chapter(String chapterName, String chapterText){
+        this.chapterName = chapterName;
+        this.chapterText = chapterText;
+    }
+
+    public String getChapterName () {return chapterName;}
+    public String getChapterText () {return chapterText;}
+}
