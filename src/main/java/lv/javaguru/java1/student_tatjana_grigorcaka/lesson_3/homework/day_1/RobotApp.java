d5e4e4a2c701343bfea3a76b54fbbadea632549d
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-22 21:39:12 +0100

    lesson 3 homework robot

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/homework/day_1/RobotApp.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.homework.day_1;
+
+public class RobotApp {
+
+    public static void main(String[] args) {
+        Robot alphaRobot = new Robot("Alpha");
+        alphaRobot.setName("Ug");
+        System.out.println(alphaRobot.getName());
+
+        Robot jarvisRobot = new Robot("Jarvis");
+        jarvisRobot.setName("Jet");
+        System.out.println(jarvisRobot.getName());
+    }
+
+    }
+
