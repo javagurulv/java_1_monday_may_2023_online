1874983f6135adee2e33c7e98cdbe19f9ca282a7
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-23 13:03:33 +0100

    lesson 3 homework car app final updated

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/homework/day_2/Car.java
@@ -0,0 +1,27 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.homework.day_2;
+
+public class Car {
+
+    private String brand;
+    private String color;
+    private int price;
+
+    public Car(String brand, String color, int price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+
+
+    public String getBrand() {return brand;}
+
+    public void setBrand(String brand) {this.brand = brand;}
+
+    public String getColor() {return color;}
+
+    public void setColor(String color) {this.color = color;}
+
+    public double getPrice() {return price;}
+    public void setPrice(int price) {this.price = price;}
+
+}
