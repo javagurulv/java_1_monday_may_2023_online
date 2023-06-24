929687ffaf376df89dde2184c08a3d15979960c9
Alex-Likurg <likurg68@gmail.com>
2023-06-24 22:12:37 +0300

    The First day task

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/homework/Day1/Book.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.homework.Day1;
+
+// Задаю класс к началу работы.Ошибка в незакрытых скобках в конце.
+public class Book {
+    public static void main(String[]args) {
+    }
+}
+
+
+
---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/homework/Day1/Turtle.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.homework.Day1;
+
+// Задаю класс к началу работы.Ошибка в отсутствии открывающей скобки.
+public class Turtle {
+    public static void main(String[]args) {
+    }
+}
