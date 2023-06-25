ba4c4604cadf988495b36baafab85a55a95f5b1d
Alex-Likurg <likurg68@gmail.com>
2023-06-25 17:51:31 +0300

    Lesson3 Student Mark

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_3/workinclass/StudentMark.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_3.workinclass;
+
+public class StudentMark {
+
+    String name;
+    String surname;
+    String subject;
+    int mark;
+}
