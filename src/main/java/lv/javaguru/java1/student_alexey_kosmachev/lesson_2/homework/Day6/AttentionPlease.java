fccf40b4698061445a1f00fdd971cd8cbfbaf028
Alex-Likurg <likurg68@gmail.com>
2023-06-25 10:58:41 +0300

    Day6.Attention Please. Find mistake.

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_2/homework/Day6/AttentionPlease.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_2.homework.Day6;
+//Заставьте эту программу работать!
+//
+//class AttentionPlease {
+//
+//    System.out.println("Code outside the method will not compile!");
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
+//
+//Цель этого задания: обратить ваше внимания на то, где должен
+//распологаться программный код. В языке программирования Java
+//программный код с командами должен находиться внутри метода.
+public class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!"); //Командный код system.out помещен внутри
+
+    }
+}
+
