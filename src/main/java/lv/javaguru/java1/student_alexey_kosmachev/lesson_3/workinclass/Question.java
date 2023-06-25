1f9485c0bafcd48672c9ccd70d9b1299e8544ff5
Alex-Likurg <likurg68@gmail.com>
2023-06-25 18:56:34 +0300

    Lesson3 Question and QuestionnaireApp

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_3/workinclass/Question.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_3.workinclass;
+
+public class Question {
+    String questionText;
+    boolean answer;
+
+    public Question(String questionText) {
+        this.questionText = questionText;
+    }
+
+}
