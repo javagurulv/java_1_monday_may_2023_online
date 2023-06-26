5da02a74ca72875c9ad8e43f444eaa5653c27fce
javagurulv <javagurulv@gmail.com>
2023-06-26 19:41:06 +0300

    tasks

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_4_if_statement/lessoncode/NegativeNumber.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.teacher.lesson_4_if_statement.lessoncode;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class NegativeNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+        boolean isNegative = number < 0;
+        System.out.println("Number is negative = " + isNegative);
+    }
+
+}
