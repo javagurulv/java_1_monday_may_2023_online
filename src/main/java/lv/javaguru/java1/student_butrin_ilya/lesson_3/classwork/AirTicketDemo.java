9221acf0ba978295ddfe7548f818b9890e7d38f8
javagurulv <javagurulv@gmail.com>
2023-06-26 14:18:18 +0300

    code compilation fix

--- src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_3/classwork/AirTicketDemo.java
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_3/classwork/AirTicketDemo.java
@@ -6,7 +6,7 @@ import java.util.GregorianCalendar;
 
 public class AirTicketDemo {
     public static void main(String[] args) {
-        Calendar air1 = new GregorianCalendar((2017, 0 , 25, 20, 30);
+        Calendar air1 = new GregorianCalendar(2017, 0 , 25, 20, 30);
         Calendar air2  = new GregorianCalendar(2012, 3,13, 20, 40);
         AirTicket air = new AirTicket("Moskva", "spb", air1.getTime(), air2.getTime());
   System.out.println(air.getTimeDateStart());
