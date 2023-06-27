b230ad6a982bb6d01f2c674628a746f3e33074ba
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-27 21:03:36 +0100

    lesson 3 homework day_7 dog

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/homework/day_7/DogDemo.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.homework.day_7;
+
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dogOne = new Dog("Rex", 3);
+        dogOne.getName();
+        dogOne.getAge();
+        dogOne.getCelebration();
+        dogOne.getAge();
+        dogOne.getCelebration();
+        dogOne.getAge();
+        dogOne.getCelebration();
+        dogOne.getAge();
+    }
+}
+
+
