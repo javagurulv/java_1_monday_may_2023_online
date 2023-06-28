e579eaa39faf89f7e70eb5070e4df67dba8d17a4
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-28 22:41:12 +0100

    lesson 3 lessoncode tax 12v& 13 questionnaire

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/Questionnaire.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+public class Questionnaire {
+    String name;
+    Question questionOne;
+    Question questionTwo;
+
+//constructor
+
+    public Questionnaire(String name, Question questionOne, Question questionTwo)  {
+        this.name = name;
+        this.questionOne = questionOne;
+        this.questionTwo = questionTwo;
+
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_3/lessoncode/QuestionnaireApp.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_3.lessoncode;
+
+public class QuestionnaireApp {
+    public static void main(String[] args) {
+        Question questionAboutYourself = new Question("All good?");
+        Question questionAboutJavaGuru = new Question("All fine?");
+        Questionnaire questionnaire = new Questionnaire("Alex",questionAboutYourself, questionAboutJavaGuru);
+
+
+        System.out.println(questionnaire.questionOne.questionText);
+        System.out.println(questionnaire.questionOne.answer);
+
+        System.out.println(questionnaire.questionTwo.questionText);
+        System.out.println(questionnaire.questionTwo.answer);
+    }
+
+}
