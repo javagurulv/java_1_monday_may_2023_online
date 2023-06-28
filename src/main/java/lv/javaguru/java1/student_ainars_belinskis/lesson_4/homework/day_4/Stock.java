05fa70911cefd38052a16460cc6cdde9dd810ec7
AinarsB <abelinskis@gmail.com>
2023-06-29 01:34:02 +0300

    Lesson_4 homework day_4

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_4/homework/day_4/Stock.java
@@ -0,0 +1,40 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_4.homework.day_4;
+
+public class Stock {
+    private String kompanyName;
+    private double presentPrice;
+    private double minPrice;
+    private double maxPrice;
+
+    public Stock(String kompanyName, double presentPrice) {
+        this.kompanyName = kompanyName;
+        this.presentPrice = presentPrice;
+    }
+
+    public void setPresentPrice(double presentPrice) {
+        this.presentPrice = presentPrice;
+
+    }
+
+    public void updatePrice() {
+        if (minPrice == 0) {
+            this.minPrice = presentPrice;
+        }
+         if (maxPrice == 0) {
+            this.maxPrice = presentPrice;
+        }
+         if (maxPrice <= presentPrice) {
+            this.maxPrice = presentPrice;
+        }
+        if (minPrice >= presentPrice) {
+            this.minPrice = presentPrice;
+        }
+
+    }
+
+    public String getPriceInformation() {
+        return "Kompany: " + kompanyName + ", Min Price: " + minPrice + ", Present Price: " + presentPrice + ", Max Price: " + maxPrice;
+    }
+
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_4/homework/day_4/StockTest.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_4.homework.day_4;
+
+public class StockTest {
+    public static void main(String[] args) {
+        Stock bigBank = new Stock("BigBank", 10);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+        bigBank.setPresentPrice(12);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+        bigBank.setPresentPrice(5);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+        bigBank.setPresentPrice(7);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+        bigBank.setPresentPrice(99);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+        bigBank.setPresentPrice(77);
+        bigBank.updatePrice();
+        System.out.println(bigBank.getPriceInformation());
+
+    }
+}
