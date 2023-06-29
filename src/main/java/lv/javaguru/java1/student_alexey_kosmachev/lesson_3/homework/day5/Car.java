0e0e33cc733e7cb3e9193abafc3d4a12b7601ce8
Alex-Likurg <likurg68@gmail.com>
2023-06-29 12:28:29 +0300

    Day5 Car Task3 Find Mistake

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_3/homework/day5/Car.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_3.homework.day5;
+//Найти ошибку в коде.
+//Исправить и запустить программу.
+//Менять можно только класс Car.
+public class Car {
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() { //Добавить кавычки после getNodel
+        return this.model; // добавить точки с запятой в конце
+    }
+
+}
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+}
