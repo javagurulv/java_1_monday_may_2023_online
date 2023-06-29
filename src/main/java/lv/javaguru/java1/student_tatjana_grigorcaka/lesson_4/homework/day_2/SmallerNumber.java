f4408debffced60d1ca274f125db119cefc58992
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-29 23:36:57 +0100

    lesson 4 homework day 2 task 1-3 bigger num, smaller num, equal or different num

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_2/SmallerNumber.java
@@ -0,0 +1,28 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_4.homework.day_2;
+
+import java.util.Scanner;
+
+public class SmallerNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter number 2: ");
+        int number2 = scanner.nextInt();
+
+        if (number1 < number2) {
+            System.out.println("The smaller number is: " + number1);
+        }
+        else if (number2 < number1) {
+            System.out.println("The smaller number is: " + number2);
+        }
+        else {
+            System.out.println("The numbers are same");
+        }
+    }
+
+}
+
+
+
