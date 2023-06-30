1d22bc3bb19a63f4f6add9509d947703b0e0402a
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-06-30 22:45:11 +0100

    lesson 4 homework day 3 biggest num

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_3/BiggestNumber.java
@@ -0,0 +1,38 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_4.homework.day_3;
+
+import java.util.Scanner;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter number 2: ");
+        int number2 = scanner.nextInt();
+        System.out.println("Enter number 3: ");
+        int number3 = scanner.nextInt();
+
+
+        if (number1 > number2 && number1 > number3) {
+            System.out.println("The biggest number is: " + number1);
+        }
+        else if (number2 > number3 && number2 > number1) {
+            System.out.println("The biggest number is: " + number2);
+        }
+        else if (number1 == number2 && number2 == number3) {
+            System.out.println("Numbers are equal!");
+        }
+        else if (number1 > number2 && number1 == number3) {
+            System.out.println("Two equal biggest numbers: " + number1 + " and " + number3);
+        }
+        else if (number2 > number3) {
+            System.out.println("Two equal biggest numbers: " + number2 + " and " +  number1);
+        }
+        else if (number3 == number2) {
+            System.out.println("Two equal biggest numbers: " + number3 + " and " +  number2);
+        }
+        else
+            System.out.println("The biggest number is: "  + number3);
+        }
+    }
+
