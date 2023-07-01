56f11ad379502ff285a9f732c0710e1ccb181db5
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-01 12:57:05 +0100

    lesson 4 homework day 6 seasons app updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/SeasonsApp.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/SeasonsApp.java
@@ -8,32 +8,44 @@ public class SeasonsApp {
         System.out.println("Enter month number (1 to 12): ");
         int number = sc.nextInt();
         switch (number) {
-            case (1) ->
-                    System.out.println("January is in the Winter.");
-            case (2) ->
-                System.out.println("February is in the Winter.");
-            case (3) ->
-                System.out.println("March is in the Spring.");
-            case (4) ->
-                System.out.println("April is in the Spring.");
-            case (5) ->
-                System.out.println("May is in the Spring.");
-            case (6) ->
-                System.out.println("June is in the Summer.");
-            case (7) ->
-                System.out.println("July is in the Summer.");
-            case (8) ->
-                System.out.println("August is in the Summer.");
-            case (9) ->
-                System.out.println("September is in the Autumn.");
-            case (10) ->
-                System.out.println("October is in the Autumn.");
-            case (11) ->
-                System.out.println("November is in the Autumn.");
-            case (12) ->
-                System.out.println("December is in the Winter.");
+            case (1):
+                System.out.println("January is in the Winter!");
+                break;
+            case (2):
+                System.out.println("February is in the Winter!");
+                break;
+            case (3):
+                System.out.println("March is in the  Spring!");
+                break;
+            case (4):
+                System.out.println("April is in the  Spring!");
+                break;
+            case (5):
+                System.out.println("May is in Spring!");
+                break;
+            case (6):
+                System.out.println("June is in Summer!");
+                break;
+            case (7):
+                System.out.println("July is in Summer!");
+                break;
+            case (8):
+                System.out.println("August is in Summer!");
+                break;
+            case (9):
+                System.out.println("September is in Autumn!");
+                break;
+            case (10):
+                System.out.println("October is in Autumn!");
+                break;
+            case (11):
+                System.out.println("November is in Autumn!");
+                break;
+            case (12):
+                System.out.println("December is in Winter!");
+                break;
 
-            default -> System.out.println("Invalid Month number");
+            default: System.out.println("Invalid Month number");
         }
     }
 }
