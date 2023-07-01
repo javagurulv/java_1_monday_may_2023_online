089b33e9e49d844f701613b5886d2b7caadd0df6
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-01 16:12:27 +0100

    lesson 4 homework day 6 arabic to roman & day of week switch and if updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/ArabicToRomanConverter.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/ArabicToRomanConverter.java
@@ -8,25 +8,35 @@ public class ArabicToRomanConverter {
         System.out.println("Enter Arabic numeral (1 to 10): ");
         int number = sc.nextInt();
         switch (number) {
-            case (1): System.out.println("Arabic numeral: " + number + " = Roman numeral: I");
-            break;
-            case (2): System.out.println("Arabic numeral: " + number +  " = Roman numeral: II");
-            break;
-            case (3): System.out.println("Arabic numeral: " + number +  " = Roman numeral: III");
-            break;
-            case (4): System.out.println("Arabic numeral: " + number +  " = Roman numeral: IV");
-            break;
-            case (5): System.out.println("Arabic numeral: " + number + " = Roman numeral: V");
-            break;
-            case (6): System.out.println("Arabic numeral: " + number + " = Roman numeral: ");
-            break;
-            case (7): System.out.println("Arabic numeral: " + number + " = Roman numeral: VII");
-            break;
-            case (8): System.out.println("Arabic numeral: " + number + " = Roman numeral: VIII");
-            break;
-            case (9): System.out.println("Arabic numeral: " + number + " = Roman numeral: IX");
-            break;
-            case (10): System.out.println("Arabic numeral: " + number + " = Roman numeral: X");
+            case 1:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: I");
+                break;
+            case 2:
+                System.out.println("Arabic numeral: " + number +  " = Roman numeral: II");
+                break;
+            case 3:
+                System.out.println("Arabic numeral: " + number +  " = Roman numeral: III");
+                break;
+            case 4:
+                System.out.println("Arabic numeral: " + number +  " = Roman numeral: IV");
+                break;
+            case 5:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: V");
+                break;
+            case 6:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: ");
+                break;
+            case 7:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: VII");
+                break;
+            case 8:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: VIII");
+                break;
+            case 9:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: IX");
+                break;
+            case 10:
+                System.out.println("Arabic numeral: " + number + " = Roman numeral: X");
                 break;
             default: System.out.println("Invalid number!");
                 break;
--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/DayOfWeek.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_6/DayOfWeek.java
@@ -9,39 +9,46 @@ public class DayOfWeek {
         System.out.println("Enter day number (1 to 7): ");
         int number = sc.nextInt();
         switch (number) {
-            case (1): System.out.println("Day 1 is Monday");
-            break;
-            case (2): System.out.println("Day 2 is Thursday");
-            break;
-            case (3): System.out.println("Day 3 is Wednesday");
-            break;
-            case (4): System.out.println("Day 4 is Thursday");
-            break;
-            case (5): System.out.println("Day 5 is in Friday");
-            break;
-            case (6): System.out.println("Day 6 is Saturday");
-            break;
-            case (7): System.out.println("Day 7 is Sunday!");
-            break;
+            case 1:
+                System.out.println("Day 1 is Monday");
+                break;
+            case 2:
+                System.out.println("Day 2 is Thursday");
+                break;
+            case 3:
+                System.out.println("Day 3 is Wednesday");
+                break;
+            case 4:
+                System.out.println("Day 4 is Thursday");
+                break;
+            case 5:
+                System.out.println("Day 5 is in Friday");
+                break;
+            case 6:
+                System.out.println("Day 6 is Saturday");
+                break;
+            case 7:
+                System.out.println("Day 7 is Sunday!");
+                break;
             default: System.out.println("Invalid day number!");
-            break;
+                break;
         }
 
-            if (number == 1) {
+        if (number == 1) {
             System.out.print("Monday");
-            } else if (number == 2) {
+        } else if (number == 2) {
             System.out.print("Tuesday");
-            } else if (number == 3) {
+        } else if (number == 3) {
             System.out.print("Wednesday");
-            } else if (number == 4) {
+        } else if (number == 4) {
             System.out.print("Thursday");
-            } else if (number == 5) {
+        } else if (number == 5) {
             System.out.print("Friday");
-            } else if (number == 6) {
+        } else if (number == 6) {
             System.out.print("Saturday");
-            } else if (number == 7) {
+        } else if (number == 7) {
             System.out.print("Sunday");
-            } else {
+        } else {
             System.out.print("Invalid number");
         }
     }
