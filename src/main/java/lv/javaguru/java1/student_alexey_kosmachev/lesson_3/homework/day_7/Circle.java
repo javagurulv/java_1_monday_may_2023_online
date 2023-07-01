55c526ce65d22ece8ac4ba929e04750eb38a56ac
Alex-Likurg <likurg68@gmail.com>
2023-07-01 18:15:04 +0300

    Day 7 Circle and Circle Demo

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_3/homework/day_7/Circle.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_3.homework.day_7;
+
+public class Circle {
+
+    double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+        public double calculateArea() { return Math.PI * radius * radius; }
+}
+
+
