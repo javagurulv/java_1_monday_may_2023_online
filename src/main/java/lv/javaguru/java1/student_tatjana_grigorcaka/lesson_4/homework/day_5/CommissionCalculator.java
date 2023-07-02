cb5646d973711222683ddcc85299b6fa1e1857ab
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-02 14:52:59 +0100

    lesson 4 homework commission calculator updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_5/CommissionCalculator.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_4/homework/day_5/CommissionCalculator.java
@@ -3,23 +3,22 @@ package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_4.homework.day_5;
 
 import java.util.Scanner;
 
-public class CommissionCalculator {
+ class CommissionCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter withdrawal amount: ");
         double amount = scanner.nextInt();
 
-
         if (amount < 1000) {
             double commission1 = (amount * 3) / 100;
-            System.out.println("Commission amount is: " + commission1);
-        }else if ((amount >= 1000) && (amount < 5000)) {
+            System.out.println("Commission amount is 3% - " + commission1);
+        }else if ((amount >= 1000) && (amount <= 5000)) {
             double commission2 = (amount * 4) / 100;
-            System.out.println("Commission amount  is: " + commission2);
-        }else if (amount >= 5000) {
+            System.out.println("Commission amount  is 4%: " + commission2);
+        }else if (amount > 5000) {
             double commission3 = (amount * 5) / 100;
-            System.out.println("Commission amount is: " + commission3);
+            System.out.println("Commission amount is 5%: " + commission3);
 
         }
     }
