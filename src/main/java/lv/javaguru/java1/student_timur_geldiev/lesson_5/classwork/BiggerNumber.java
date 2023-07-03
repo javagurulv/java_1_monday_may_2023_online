6730a54ccf12a62c8836dfeffe620ad5dc195f0c
TG_B <timur.geldiev@gmail.com>
2023-07-03 21:09:05 +0300

    Lesson 5 classwork task5

--- src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/BiggerNumber.java
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/BiggerNumber.java
@@ -1,7 +1,12 @@
 package lv.javaguru.java1.student_timur_geldiev.lesson_5.classwork;
 
-class BiggerNumber {
+public class BiggerNumber {
     public int findBigger(int number1, int number2) {
         return (number1 > number2) ? number1 : number2;
+    }
+    public int findMax(int number1, int number2, int number3){
+        int max = findBigger(number1, number2);
+        max = findBigger(max, number3);
+        return max;
     }
 }
