f213c51f6d05b849a3410921272afeee91ae464a
Alex-Likurg <likurg68@gmail.com>
2023-07-03 12:19:14 +0300

    lesson 4 homework day2 Numbers equal or different. Task 3.

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_4/homework/day_2/EqualNumber.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_4.homework.day_2;
+
+import java.util.Scanner;
+
+class EqualNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int firstnumber = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int secondnumber = scanner.nextInt();
+
+        if (secondnumber == firstnumber) {System.out.println("Numbers are equals");}
+        if (secondnumber != firstnumber) {System.out.println("Numbers are different");}
+
+    }
+}
