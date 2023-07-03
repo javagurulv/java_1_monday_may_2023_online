48369e657a81f9cf7c2202dc7f65462ab4474428
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-03 21:38:26 +0100

    lesson 5 lessoncode triangle square

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/lessoncode/TriangleSquareApp.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.lessoncode;
+
+class TriangleSquareApp {
+
+    public static void main(String[] args) {
+    TriangleSquare triangleSquare = new TriangleSquare();
+    System.out.println("Semi perimeter = " + triangleSquare.calculateSemiPerimeter(2, 3, 4));
+
+    }
+
+}
