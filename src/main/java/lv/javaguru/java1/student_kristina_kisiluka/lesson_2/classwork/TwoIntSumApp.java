795648be9218bd914d97fcf843b1745c0e25be56
kpucmuhhka <kristinakisiluka@gmail.com>
2023-07-04 02:33:05 +0300

    Lesson_2; first tasks;

---
+++ src/main/java/lv/javaguru/java1/student_kristina_kisiluka/lesson_2/classwork/TwoIntSumApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_kristina_kisiluka.lesson_2.classwork;
+
+public class TwoIntSumApp {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 200;
+        int result = number1 + number2;
+        System.out.println("Sum result = " + result);
+        System.out.println(result);
+
+    }
+}
