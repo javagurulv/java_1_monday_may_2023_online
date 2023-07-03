4adda7e1a0146bb261c30bed01bf4226279c377d
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-03 20:34:21 +0100

    lesson 5 lessoncode average num calculator

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/lessoncode/AverageNumCalculator.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.lessoncode;
+
+class AverageNumCalculator {
+
+    public double findAverageDouble(int number1, int number2) {
+        return (number1 + number2) / (double) 2;
+    }
+}
+
+
