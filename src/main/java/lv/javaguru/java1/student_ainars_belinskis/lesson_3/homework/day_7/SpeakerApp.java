840094eed103fb7d70249ee006ee2c414c0c8dff
javagurulv <javagurulv@gmail.com>
2023-07-03 19:14:09 +0300

    lesson 4 recording

--- src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_3/homework/day_7/SpeakerApp.java
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_3/homework/day_7/SpeakerApp.java
@@ -2,6 +2,8 @@ package lv.javaguru.java1.student_ainars_belinskis.lesson_3.homework.day_7;
 
 class SpeakerApp {
     public static void main(String[] args) {
+        Speaker speak = new Speaker("SONY");
+
         Speaker speak1 = new Speaker("SONY", 2, true);
 
         System.out.println(speak1.getModel() + " Sound level = " + speak1.getSoundLevel() + " Switch position is: " + speak1.getSwitchOn());
