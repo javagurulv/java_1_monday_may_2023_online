b93cc297bf233afb2fe4768d9e27d507b43f8b03
TG_B <timur.geldiev@gmail.com>
2023-07-03 20:07:19 +0300

    Lesson 5 classwork task2

--- src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/EvenNumberApp.java
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/EvenNumberApp.java
@@ -2,17 +2,18 @@ package lv.javaguru.java1.student_timur_geldiev.lesson_5.classwork;
 
 public class EvenNumberApp {
     public static void main(String[] args) {
-        EvenNumber calc1 = new EvenNumber();
+        EvenNumberDetector calc1 = new EvenNumberDetector();
+        OddNumberDetector calc2 = new OddNumberDetector();
         System.out.println("The number is even = " + calc1.isEven(1));
         System.out.println("The number is even = " + calc1.isEven(2));
         System.out.println("The number is even = " + calc1.isEven(3));
         System.out.println("The number is even = " + calc1.isEven(4));
         System.out.println("The number is even = " + calc1.isEven(5));
 
-        System.out.println("The number is odd = " + calc1.isOdd(1));
-        System.out.println("The number is odd = " + calc1.isOdd(2));
-        System.out.println("The number is odd = " + calc1.isOdd(3));
-        System.out.println("The number is odd = " + calc1.isOdd(4));
-        System.out.println("The number is odd = " + calc1.isOdd(5));
+        System.out.println("The number is odd = " + calc2.isOdd(1));
+        System.out.println("The number is odd = " + calc2.isOdd(2));
+        System.out.println("The number is odd = " + calc2.isOdd(3));
+        System.out.println("The number is odd = " + calc2.isOdd(4));
+        System.out.println("The number is odd = " + calc2.isOdd(5));
     }
 }
---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/OddNumberDetector.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_5.classwork;
+
+public class OddNumberDetector {
+    public boolean isOdd(int number){
+        return number %2 != 0;
+    }
+}
