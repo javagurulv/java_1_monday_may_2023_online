f0373f3768b681cca33d93b83a5d381c2df13c40
kpucmuhhka <kristinakisiluka@gmail.com>
2023-07-04 19:25:07 +0300

    Lesson_2; first tasks;

--- src/main/java/lv/javaguru/java1/student_kristina_kisiluka/lesson_2/classwork/EvenOddNumberApp.java
+++ src/main/java/lv/javaguru/java1/student_kristina_kisiluka/lesson_2/classwork/EvenOddNumberApp.java
@@ -2,16 +2,13 @@ package lv.javaguru.java1.student_kristina_kisiluka.lesson_2.classwork;
 import java.util.Scanner;
 public class EvenOddNumberApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // создаём объект класса Scanner
-        int i = 0;
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        if (sc.hasNextInt()) {
-            i = sc.nextInt();
-            if (i%2 == 0){
-                System.out.println("integer" + " " + i + " " + "even");
-            } else {
-                System.out.println("You entered an odd integer");
-            }
+        int number= sc.nextInt();
+        if (number % 2 == 0){
+            System.out.println("Your integer is even");
+        }else {
+            System.out.println("Your integer is odd");
         }
     }
 }
