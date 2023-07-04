9e5d0e2d52927f9ce5b1c9b0f052b0fca31cbc08
TG_B <timur.geldiev@gmail.com>
2023-07-04 11:08:20 +0300

    Lesson 5 classwork

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_5/classwork/Message.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_5.classwork;
+
+public class Message {
+    String message;
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
