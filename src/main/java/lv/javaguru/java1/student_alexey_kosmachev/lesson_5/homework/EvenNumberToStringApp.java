a8a3e4034a78773f7d6252f4fae439756a7f6e67
Alex-Likurg <likurg68@gmail.com>
2023-07-05 20:08:02 +0300

    lesson5 day1 Task1 Turn nuber into String

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/EvenNumberToStringApp.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_5.homework;
+
+public class EvenNumberToStringApp {
+    public static void main(String[] args) {
+
+        EvenNumberToString newnumber = new EvenNumberToString();
+        System.out.println("Turn number to string result: " + newnumber.yournumber1(9));
+        System.out.println("Turn number to string result: " + newnumber.yournumber1(20));
+        System.out.println("Turn number to string result: " + newnumber.yournumber2(10));
+        System.out.println("Turn number to string result: " + newnumber.yournumber2(14));
+        System.out.println("Turn number to string result: " + newnumber.yournumber3(15));
+        System.out.println("Turn number to string result: " + newnumber.yournumber3(16));
+
+
+    }
+}
