ea54bbc19155a81832bb171b45561d69a6252972
AinarsB <abelinskis@gmail.com>
2023-07-07 01:11:42 +0300

    Lesson_5 homework day_2

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_2/LeapYearApp.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_5.homework.day_2;
+
+import java.util.Scanner;
+
+public class LeapYearApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year: ");
+        int year = sc.nextInt();
+        LeapYearDetector leapYearDetector = new LeapYearDetector();
+
+        System.out.println("The " + year + " year is leap: " + leapYearDetector.isLeapYear(year));
+        System.out.println("The " + (year + 1) + " year is leap: " + leapYearDetector.isLeapYear(year +1));
+        System.out.println("The " + (year + 2) + " year is leap: " + leapYearDetector.isLeapYear(year + 2));
+        System.out.println("The " + (year + 3) + " year is leap: " + leapYearDetector.isLeapYear(year + 3));
+    }
+}
