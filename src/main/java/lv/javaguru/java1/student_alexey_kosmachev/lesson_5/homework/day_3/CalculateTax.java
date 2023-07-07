05b8aaaf231909965597cde886066ada3f4cb042
Alex-Likurg <likurg68@gmail.com>
2023-07-07 11:39:51 +0300

    lesson5 day3 Task1
    Calculate tax method

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/day_3/CalculateTax.java
@@ -0,0 +1,27 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_5.homework.day_3;
+
+class CalculateTax {
+
+    public double yourincome (int income) {
+
+        if (income < 10000) {
+            int tax1 = income / 100 * 30;
+            {return tax1;}
+        }
+        if ((income >= 10000) && (income < 50000)) {
+            int reminderof10k = income - 10000;
+            int tax10k = 10000 / 100 * 30;
+            int taxofReminder = reminderof10k / 100 * 40;
+            int tax2 = tax10k + taxofReminder;
+            {return tax2;}
+        }
+        else {
+            int reminderof50k = income - 50000;
+            int tax10k = 10000 / 100 * 30;
+            int tax1050k = 10000 / 100 * 40;
+            int taxofReminder = reminderof50k / 100 * 50;
+            int tax3 = tax10k + tax1050k + taxofReminder;
+            {return tax3;}
+        }
+    }
+}
