bef451c8e0e2045ba50129a558cdb5a13b77df8e
AinarsB <abelinskis@gmail.com>
2023-07-07 15:23:08 +0300

    Lesson_5 homework day_4

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_4/CirclePerimetrAreaCalculator.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_5.homework.day_4;
+
+public class CirclePerimetrAreaCalculator {
+    public double CircleArea(double radius){
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+    public double CirclePerimetr(double radius){
+        double perimetr = 2 * Math.PI * radius;
+        return perimetr;
+    }
+
+}
