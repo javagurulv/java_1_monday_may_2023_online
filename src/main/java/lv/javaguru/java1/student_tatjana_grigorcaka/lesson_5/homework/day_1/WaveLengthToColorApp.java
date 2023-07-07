f4bd819117a8fae318496ba557899c628a1f598c
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-08 00:27:13 +0100

    lesson 5 homework wave length to color

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_1/WaveLengthToColorApp.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.homework.day_1;
+
+class WaveLengthToColorApp {
+
+    public static void main(String[] args) {
+        WaveLengthToColor waveLengthToColor = new WaveLengthToColor();
+
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(380));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(494));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(568));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(570));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(588));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(590));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(619));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(621));
+        System.out.println(waveLengthToColor.convertWaveLenghtToColor(751));
+    }
+}
