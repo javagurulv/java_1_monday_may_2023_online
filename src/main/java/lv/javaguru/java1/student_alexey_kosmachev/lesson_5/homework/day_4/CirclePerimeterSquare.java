0cfae669dc2a94ec50083551530d148fa59cbd58
Alex-Likurg <likurg68@gmail.com>
2023-07-08 18:20:00 +0300

    lesson5 day4 Task1
    Calculate Perimeter and Sqare method

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/day_4/CirclePerimeterSquare.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_5.homework.day_4;
+
+class CirclePerimeterSquare {
+
+    public double perimeter (int radius) {                         //метод определения периметра
+        double calculatePerimeter = 2 * Math.PI * radius;
+        { return calculatePerimeter;}
+    }
+
+
+    public double square (int radius) {                            // метод определения площади
+        double calculateSqare = Math.PI * Math.sqrt(radius);
+        { return calculateSqare;}
+    }
+}
+
+
+
---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/day_4/CirclePerimeterSquareApp.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_5.homework.day_4;
+
+public class CirclePerimeterSquareApp {
+
+    public static void main(String[] args) {
+
+        CirclePerimeterSquare newdefinition = new CirclePerimeterSquare();
+
+        System.out.println("The Perimeter is: " + newdefinition.perimeter(6));
+        System.out.println("The Square is: " + newdefinition.square(6));
+        System.out.println("The Perimeter is: " + newdefinition.perimeter(35));
+        System.out.println("The Square is: " + newdefinition.square(35));
+        System.out.println("The Perimeter is: " + newdefinition.perimeter(12));
+        System.out.println("The Square is: " + newdefinition.square(12));
+        System.out.println("The Perimeter is: " + newdefinition.perimeter(18));
+        System.out.println("The Square is: " + newdefinition.square(18));
+    }
+}
