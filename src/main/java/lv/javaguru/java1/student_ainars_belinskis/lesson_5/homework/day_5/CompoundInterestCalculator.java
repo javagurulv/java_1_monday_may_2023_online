15c2b4aad773522636e33d48f64793560b6b0894
AinarsB <abelinskis@gmail.com>
2023-07-09 10:54:19 +0300

    Lesson_5 homework day_5

--- src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_5/CompoundInterestCalculator.java
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_5/CompoundInterestCalculator.java
@@ -4,8 +4,8 @@ package lv.javaguru.java1.student_ainars_belinskis.lesson_5.homework.day_5;
 //S = S0 × (1 + P / 100)n, где S — финальная сумма вклада S0 — начальная сумма вклада
 // P — процентная ставка с учетом капитализации n — количество периодов, в которые банк начисляет проценты.
 class CompoundInterestCalculator {
-    public double CompoundInterest(double summa, double interest, double year){
-                return  summa * Math.pow((1 + interest / 100), year);
+    public double CompoundInterest(double sum, double interest, double year){
+                return  sum * Math.pow((1 + interest / 100), year);
     }
 }
 
