419401b4cfa448a4d0fe2650d354dcbf18fa0761
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-09 12:12:49 +0100

    lesson 5 homework day 4 circle perimeter & square

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_4/CirclePerimeterSquare.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.homework.day_4;
+
+class CirclePerimeterSquare {
+    public double calculateCirclePerimeter (double radius) { return 2 * Math.PI * radius; }
+
+    public double calculateCircleSquare (double radius) { return Math.PI * radius * radius; }
+}
