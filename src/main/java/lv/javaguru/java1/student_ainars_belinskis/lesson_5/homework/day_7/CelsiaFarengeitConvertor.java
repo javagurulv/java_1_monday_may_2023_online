3c1b9a41856eb505632494f0e38cc2322778549a
AinarsB <abelinskis@gmail.com>
2023-07-09 10:59:41 +0300

    Lesson_5 homework day_7

--- src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_7/CelsiaFarengeitConvertor.java
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_5/homework/day_7/CelsiaFarengeitConvertor.java
@@ -2,7 +2,7 @@ package lv.javaguru.java1.student_ainars_belinskis.lesson_5.homework.day_7;
 
 class CelsiaFarengeitConvertor {
     public double CelsiaToFarengeit(double celsia) {
-        return celsia* 1.8 + 32;
+        return celsia * 1.8 + 32;
     }
 
     public double FarengeitToCelsia(double farengeit) {
