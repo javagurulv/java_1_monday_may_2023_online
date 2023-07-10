59b6c353e23d9c73d0f07d2fae3f1f36ef450e8a
javagurulv <javagurulv@gmail.com>
2023-07-10 19:20:37 +0300

    code review

--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/FizzBuzz.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/FizzBuzz.java
@@ -4,11 +4,15 @@ class FizzBuzz {
 
     public String translateNumberToString(int number) {
         String result;
-        if (isDevidedBy3(number) && isDevidedBy5(number)) result = "fizzBuzz";
+        if (isDevidedBy15(number)) result = "fizzBuzz";
         else if (isDevidedBy3(number)) result = "fizz";
         else if (isDevidedBy5(number)) result = "Buzz";
         else result = "" + number;
         return result;
+    }
+
+    private boolean isDevidedBy15(int number) {
+        return isDevidedBy3(number) && isDevidedBy5(number);
     }
 
     private boolean isDevidedBy5(int number) {
