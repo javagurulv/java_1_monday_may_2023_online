256b6020c5197e577ab7e44e580b4382888b0aa1
javagurulv <javagurulv@gmail.com>
2023-07-10 21:09:26 +0300

    lesson 6 class code

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_6_unit_testing/lessoncode/MaxOfTwoNumbers.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.teacher.lesson_6_unit_testing.lessoncode;
+
+class MaxOfTwoNumbers {
+
+    public int max(int number1, int number2) {
+        if (number1 > number2) {
+            return number1;
+        } else {
+            return number2;
+        }
+    }
+
+}
