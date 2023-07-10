3de94bd880c33f875ab61f28352a7bcdca22f862
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-10 21:07:02 +0100

    lessoncode 6 even & odd number test

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_6/lessoncode/OddNumberTest.java
@@ -0,0 +1,33 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_6.lessoncode;
+
+class OddNumberTest {
+
+    public static void main(String[] args) {
+        OddNumberTest test = new OddNumberTest();
+        test.testIsEvenNumber();
+        test.testIsOddNumber();
+    }
+
+    public void testIsOddNumber() {
+        int number = 5;
+        OddNumber oddNumber = new OddNumber();
+        boolean result = oddNumber.isOdd(number);
+        if (result == true) {
+            System.out.println("TEST OK!");
+        } else {
+            System.out.println("TEST FAIL!");
+        }
+    }
+
+    public void testIsEvenNumber() {
+        int number = 4;
+        OddNumber oddNumber = new OddNumber();
+        boolean result = oddNumber.isOdd(number);
+        if (result == false) {
+            System.out.println("TEST OK!");
+        } else {
+            System.out.println("TEST FAIL!");
+        }
+    }
+
+}
