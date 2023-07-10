eb518396572e99c555c8e5d3d970ff3a1b9840af
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-10 01:30:06 +0100

    lesson 5 homework day 5 compound interest

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_5/CompoundInterest.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_5/CompoundInterest.java
@@ -1,17 +1,10 @@
 package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.homework.day_5;
 
-//Formula to calculate compound interest annually is given by:
-//Compound Interest = P(1 + R/100)t
-//Where,
-//P is principal amount
-//R is the rate and
-//T is the time span
-
 class CompoundInterest {
+
     public double calculateCompoundInterest(double principal, double rate, double year) {
-        return principal * (Math.pow((1 + rate / 100), year));
+            return principal * (Math.pow((1 + rate / 100), year)); }
     }
-}
 
 
 
--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_5/CompoundInterestApp.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_5/homework/day_5/CompoundInterestApp.java
@@ -1,12 +1,12 @@
 package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_5.homework.day_5;
 
-
 import java.util.Scanner;
 
-public class CompoundInterestApp {
+class CompoundInterestApp {
     public static void main(String[] args) {
         CompoundInterest compoundInterest1 = new CompoundInterest();
-        System.out.println("Total amount is: " + compoundInterest1.calculateCompoundInterest(1200, 5.4, 2));
-        System.out.println("Total amount is: " + compoundInterest1.calculateCompoundInterest(100000, 5, 3));
-       }
+        System.out.println("The total amount is: " + compoundInterest1.calculateCompoundInterest(2000,5,3));
+        System.out.println("The total amount is: " + compoundInterest1.calculateCompoundInterest(5000,5,5));
+    }
 }
+
