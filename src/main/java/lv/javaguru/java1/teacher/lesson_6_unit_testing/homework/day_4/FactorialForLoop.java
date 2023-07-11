54fd7dbad88da0e24ed77a8f8f523c9922720fd8
javagurulv <javagurulv@gmail.com>
2023-07-11 09:47:08 +0300

    lesson 6 class code

--- src/main/java/lv/javaguru/java1/teacher/lesson_6_unit_testing/homework/day_4/FactorialForLoop.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_6_unit_testing/homework/day_4/FactorialForLoop.java
@@ -1,6 +1,6 @@
 package lv.javaguru.java1.teacher.lesson_6_unit_testing.homework.day_4;
 
-class FactorialCalculator {
+class FactorialForLoop {
 
     public int factorial(int n) {
         int result = 1;
