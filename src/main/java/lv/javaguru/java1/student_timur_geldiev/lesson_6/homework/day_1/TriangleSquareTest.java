730ec88d04b4088611e4095c566021ba76cf8e77
TG_B <timur.geldiev@gmail.com>
2023-07-12 12:21:30 +0300

    Lesson 6 homework day1

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_6/homework/day_1/TriangleSquareTest.java
@@ -0,0 +1,36 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_6.homework.day_1;
+
+import lv.javaguru.java1.student_timur_geldiev.lesson_5.classwork.TriangleSquare;
+
+public class TriangleSquareTest {
+    int a = 6;
+    int b = 7;
+    int c = 8;
+
+    public static void main(String[] args) {
+        TriangleSquareTest test1 = new TriangleSquareTest();
+        test1.squareCalculation();
+        test1.semiPerimeterCalculation();
+    }
+
+    public void squareCalculation() {
+        TriangleSquare check1 = new TriangleSquare();
+        double actual = check1.calculateSquare(a, b, c);
+        checkResult(actual, 20.33316256758894, "SquareCalculationCheck");
+
+    }
+
+    public void semiPerimeterCalculation() {
+        TriangleSquare check2 = new TriangleSquare();
+        double actual = check2.findSemiPerimeter(a, b, c);
+        checkResult(actual, 10.5, "SemiPerimeterCheck");
+    }
+
+    public void checkResult(double actualResult, double expectedResult, String testName) {
+        if (actualResult == expectedResult) {
+            System.out.println(testName + " Test PASS");
+        } else {
+            System.out.println(testName + " Test FAIL");
+        }
+    }
+}
