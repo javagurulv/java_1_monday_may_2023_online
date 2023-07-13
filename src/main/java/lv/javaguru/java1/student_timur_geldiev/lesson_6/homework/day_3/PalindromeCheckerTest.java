eea06908ea84b4a84155413b16577d9a0e42e479
TG_B <timur.geldiev@gmail.com>
2023-07-13 12:45:45 +0300

    Lesson 6 homework day3

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_6/homework/day_3/PalindromeCheckerTest.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_6.homework.day_3;
+
+public class PalindromeCheckerTest {
+    public static void main(String[] args) {
+        PalindromeCheckerTest test = new PalindromeCheckerTest();
+        test.checkIfPalindrome();
+    }
+    public void checkIfPalindrome(){
+        String stringForTest = "Tenet";
+        PalindromeChecker check = new PalindromeChecker();
+        boolean actual = check.isPalindrome(stringForTest);
+        checkResult(actual, true, "Check if palindrome" );
+    }
+    public void checkResult(boolean actualResult, boolean expectedResult, String testName) {
+        if (actualResult == expectedResult) {
+            System.out.println(testName + " Test PASS");
+        } else {
+            System.out.println(testName + " Test FAIL");
+        }
+    }
+}
