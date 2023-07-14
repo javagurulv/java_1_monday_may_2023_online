507c87894190b8eeffdbbca3844d5fc626ec009b
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-14 11:39:03 +0100

    homework 6 day 2  max of three numbers test updated homework 6 day 2  square equation test updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_6/homework/day_2/MaxOfThreeNumbersTest.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_6/homework/day_2/MaxOfThreeNumbersTest.java
@@ -1,17 +1,41 @@
 package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_6.homework.day_2;
 
 class MaxOfThreeNumbersTest {
-    //Test data 1: num1 = 5, num2 = 7, num3 = 14; expected result: 14
+    //Test data 1: num1 = 15, num2 = 7, num3 = 2; expected result: 15
+    //Test data 2: num1 = 2, num2 = 8, num3 = -14; expected result: 8
+    //Test data 3:  num1 = 0, num2 = 5, num3 = 11; expected result: 11
+    //Test data 4: num1 = 14, num2 = 14, num3 = 14; expected result: 14
 
     public static void main(String[] args) {
         MaxOfThreeNumbersTest test = new MaxOfThreeNumbersTest();
-        test.testFindMax1();
+        test.testFindMaxFirstNumBigger1();
+        test.testFindMaxSecondNumBigger2();
+        test.testFindMaxThirdNumBigger3();
+        test.testFindMaxNumEqual4();
     }
 
-    public void testFindMax1() {
+    public void testFindMaxFirstNumBigger1() {
         MaxOfThreeNumbers maxOfThreeNumbers = new MaxOfThreeNumbers();
-        int result = maxOfThreeNumbers.findMax(5, 7, 14);
-        checkResult(result, 14, "testFindMax1");
+        int result = maxOfThreeNumbers.findMax(15, 7, 2);
+        checkResult(result, 15, "testFindMaxFirstNumBigger1");
+    }
+
+    public void testFindMaxSecondNumBigger2() {
+        MaxOfThreeNumbers maxOfThreeNumbers = new MaxOfThreeNumbers();
+        int result = maxOfThreeNumbers.findMax(2, 8, -14);
+        checkResult(result,8, "testFindMaxSecondNumBigger2");
+    }
+
+    public void testFindMaxThirdNumBigger3() {
+        MaxOfThreeNumbers maxOfThreeNumbers = new MaxOfThreeNumbers();
+        int result = maxOfThreeNumbers.findMax(0, 5, 11);
+        checkResult(result,11, "testFindMaxThirdNumBigger3");
+    }
+
+    public void testFindMaxNumEqual4() {
+        MaxOfThreeNumbers maxOfThreeNumbers = new MaxOfThreeNumbers();
+        int result = maxOfThreeNumbers.findMax(14, 14, 14);
+        checkResult(result, 14, "testFindMaxNumEqual4");
     }
 
     private void checkResult(double realResult,
--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_6/homework/day_2/SquareEquationTest.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_6/homework/day_2/SquareEquationTest.java
@@ -3,15 +3,17 @@ package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_6.homework.day_2;
 class SquareEquationTest {
     //Test data 1: a = 1, b = 5, c = 2; expected result = 17
     //Test data 2: a = 1, b = 5, c = 2; expected result = true
-    //Test data 3: a = 2, b = 3, c = 4; expected result = -4.561552812808831
-    //Test data 4: a = 2, b = 3, c = 4; expected result = -0.4384471871911697
+    //Test data 3: a = 1, b = 5, c = 2; expected result = -4.561552812808831
+    //Test data 4: a = 1, b = 5, c = 2; expected result = -0.4384471871911697
+    //Test data 5: a = 5, b = 1, c = 1; expected result = false
 
     public static void main(String[] args) {
         SquareEquationTest test = new SquareEquationTest();
         test.testDiscriminant1();
-        test.testIsSquareRoot2();
+        test.testIsSquareRootTrue2();
         test.testFirstRoot3();
         test.testSecondRoot4();
+        test.testIsSquareRootFalse5();
     }
 
     public void testDiscriminant1() {
@@ -20,10 +22,10 @@ class SquareEquationTest {
         checkResultDouble(result, 17, "testDiscriminant1");
     }
 
-    public void testIsSquareRoot2() {
+    public void testIsSquareRootTrue2() {
         SquareEquation squareEquation = new SquareEquation();
         boolean result = squareEquation.isSquareRoot(1, 5, 2);
-        checkResultBoolean(result, true, "testIsSquareRoot2");
+        checkResultBoolean(result, true, "testIsSquareRootTrue2");
     }
 
     public void testFirstRoot3() {
@@ -36,6 +38,11 @@ class SquareEquationTest {
         SquareEquation squareEquation = new SquareEquation();
         double result = squareEquation.secondRoot(1, 5, 2);
         checkResultDouble(result, -0.4384471871911697,"testSecondRoot4");
+    }
+    public void testIsSquareRootFalse5() {
+        SquareEquation squareEquation = new SquareEquation();
+        boolean result = squareEquation.isSquareRoot(5, 1, 1);
+        checkResultBoolean(result, false, "testIsSquareRootFalse5");
     }
 
     public void checkResultDouble(double realResult,
