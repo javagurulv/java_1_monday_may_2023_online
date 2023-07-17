d14f136f90573be20c248a6bfef2850252642abf
Alex-Likurg <likurg68@gmail.com>
2023-07-17 22:26:14 +0300

    lesson7
    Work in class

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/workinclasss/Mark.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.workinclasss;
+
+class Mark {
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
