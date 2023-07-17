c7b63b4f6a348b0d127011b5bde816f3212b146c
javagurulv <javagurulv@gmail.com>
2023-07-17 22:07:07 +0300

    Lesson 6 code review

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_lists_fors/lessoncode/tasks/Mark.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_lists_fors.lessoncode.tasks;
+
+class Mark {
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_lists_fors/lessoncode/tasks/MaxMarkFinder.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_lists_fors.lessoncode.tasks;
+
+class MaxMarkFinder {
+
+    public int findMaxMark(int[] marks) {
+        int maxMark = marks[0];
+        for (int i = 0; i < marks.length; i++) {
+            int mark = marks[i];
+            if (mark > maxMark) {
+                maxMark = mark;
+            }
+        }
+        return maxMark;
+    }
+
+}
