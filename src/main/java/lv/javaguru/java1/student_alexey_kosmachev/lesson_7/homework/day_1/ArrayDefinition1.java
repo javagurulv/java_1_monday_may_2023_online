eae1a9aefa7b01557b0f71e5d1b58392582ae1ad
Alex-Likurg <likurg68@gmail.com>
2023-07-18 18:34:57 +0300

    lesson7
    day_1 Task1 Check mistakes

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_1/ArrayDefinition1.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_1;
+
+//Найти ошибку в коде
+//public static void main(String[] args) {
+//        int numbers = new int[];
+//    }
+
+
+class ArrayDefinition1 {
+
+    public int [] ArrayDefinition() {
+        int [] numbers = new int[3];
+        return numbers;
+    }
+}
