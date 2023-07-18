ec7a729305e6a84762535846a3e1c3904dc1600b
Alex-Likurg <likurg68@gmail.com>
2023-07-18 19:23:28 +0300

    lesson7
    day_1 Task2-3 Check mistakes

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_1/ArrayDefinition2.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_1;
+
+// Найдите ошибку в программе и исправить её:
+//(переменная numbers должна ссылаться на массив byte)
+//public static void main(String[] args) {
+//        int numbers = new byte[10];
+//    }
+public class ArrayDefinition2 {
+    public static void main(String[] args) {
+        byte [] numbers;
+        numbers =  new byte[10];
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_1/ArrayDefinition3.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_1;
+
+//Найдите ошибку в программе и исправить её:
+//(переменная numbers должна ссылаться на массив целых чисел)
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+    }
+}
