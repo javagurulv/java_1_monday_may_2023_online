dca2b39da025adc1f0685bc5fbcb90407ffc5f64
TG_B <timur.geldiev@gmail.com>
2023-07-19 12:49:20 +0300

    Lesson 7 homework day2

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_7/homework/day_2/RandomIntArrayApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_7.homework.day_2;
+
+class RandomIntArrayApp {
+    public static void main(String[] args) {
+
+        RandomIntArray array1 = new RandomIntArray();
+        array1.randomIntArrayPrinter(array1.randomIntArray());
+
+    }
+}
