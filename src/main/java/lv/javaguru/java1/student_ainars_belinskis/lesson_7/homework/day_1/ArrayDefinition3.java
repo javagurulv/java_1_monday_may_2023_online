e4c34bf750ffe6a5eead591a22a12c925d94cade
AinarsB <abelinskis@gmail.com>
2023-07-21 00:44:20 +0300

    Lesson_7 homework day_1

---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_7/homework/day_1/ArrayDefinition3.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_7.homework.day_1;
+
+class ArrayDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_7/homework/day_1/Arraydefinition6.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_ainars_belinskis.lesson_7.homework.day_1;
+
+class Arraydefinition6 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4};
+    }
+}
