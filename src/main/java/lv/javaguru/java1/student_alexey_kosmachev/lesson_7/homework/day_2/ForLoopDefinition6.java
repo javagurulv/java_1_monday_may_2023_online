3a637646f0a42ceb43d9a9a76d558411a8fbe3e0
Alex-Likurg <likurg68@gmail.com>
2023-07-22 19:54:32 +0300

    lesson7
    day_2 Task 6,7 Check mistakes

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_2/ForLoopDefinition6.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_2;
+
+// Найдите логическую ошибку в программе и исправить её:
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+    } // не хватало фигурной скобки
+}
+}
+
