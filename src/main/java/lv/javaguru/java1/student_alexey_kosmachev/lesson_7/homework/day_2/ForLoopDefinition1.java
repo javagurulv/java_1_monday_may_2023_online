a9f3306ea5f36334164f7de289797ac42daf042f
Alex-Likurg <likurg68@gmail.com>
2023-07-22 19:41:34 +0300

    lesson7
    day_2 Task 2 Check mistakes

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_2/ForLoopDefinition1.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_2;
+
+// Найдите синтаксическую ошибку в программе и исправьте её:
+public class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) { // i = 0
+            System.out.println(i);
+        }
+    }
+}
