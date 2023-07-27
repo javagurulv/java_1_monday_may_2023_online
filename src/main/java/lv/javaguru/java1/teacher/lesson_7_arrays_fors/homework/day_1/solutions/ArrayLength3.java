39479a8b1776b9afd285a5d86a6c3d4e2d600b0f
javagurulv <javagurulv@gmail.com>
2023-07-27 11:43:28 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_1/solutions/ArrayLength3.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_1.solutions;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+
+}
