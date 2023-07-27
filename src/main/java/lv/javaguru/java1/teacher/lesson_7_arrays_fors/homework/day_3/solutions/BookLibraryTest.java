df608edd8583140fbd3af0036f8d9d8385606c50
javagurulv <javagurulv@gmail.com>
2023-07-27 12:41:54 +0300

    Lesson 7 solutions

--- src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_3/solutions/BookLibraryTest.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_3/solutions/BookLibraryTest.java
@@ -1,11 +1,16 @@
 package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_3.solutions;
 
+import java.util.List;
+
 class BookLibraryTest {
 
     public static void main(String[] args) {
         BookLibraryTest test = new BookLibraryTest();
         test.shouldReturnZeroWhenLibraryIsEmpty();
         test.shouldReturnBookCountWhenLibraryIsNotEmpty();
+        test.shouldFoundZeroBooksWhenLibraryIsEmpty();
+        test.shouldFoundOneBook();
+        test.shouldFoundBooks();
     }
 
     public void shouldReturnZeroWhenLibraryIsEmpty() {
@@ -20,6 +25,30 @@ class BookLibraryTest {
         bookLibrary.addBook(book);
         int bookCount = bookLibrary.getBookCount();
         checkResult(bookCount, 1, "shouldReturnBookCountWhenLibraryIsNotEmpty");
+    }
+
+    public void shouldFoundZeroBooksWhenLibraryIsEmpty() {
+        BookLibrary bookLibrary = new BookLibrary();
+        List<Book> foundBooks = bookLibrary.findBooksByTitle("War");
+        checkResult(foundBooks.size(), 0, "shouldFoundZeroBooksWhenLibraryIsEmpty");
+    }
+
+    public void shouldFoundOneBook() {
+        BookLibrary bookLibrary = new BookLibrary();
+        Book book = new Book("War and peace", "Dostoevsky", 1820);
+        bookLibrary.addBook(book);
+        List<Book> foundBooks = bookLibrary.findBooksByTitle("War and peace");
+        checkResult(foundBooks.size(), 1, "shouldFoundOneBook");
+    }
+
+    public void shouldFoundBooks() {
+        BookLibrary bookLibrary = new BookLibrary();
+        Book book1 = new Book("War and peace", "Dostoevsky", 1820);
+        bookLibrary.addBook(book1);
+        Book book2 = new Book("War and peace", "Dostoevsky", 1820);
+        bookLibrary.addBook(book2);
+        List<Book> foundBooks = bookLibrary.findBooksByTitle("War and peace");
+        checkResult(foundBooks.size(), 2, "shouldFoundBooks");
     }
 
 
