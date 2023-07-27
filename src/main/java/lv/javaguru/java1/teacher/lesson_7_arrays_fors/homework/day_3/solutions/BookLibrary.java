010790c2f3111f785462b0d9b612ff09f7bb23ea
javagurulv <javagurulv@gmail.com>
2023-07-27 12:07:14 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_3/solutions/BookLibrary.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_3.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BookLibrary {
+
+    private List books = new ArrayList();
+
+}
