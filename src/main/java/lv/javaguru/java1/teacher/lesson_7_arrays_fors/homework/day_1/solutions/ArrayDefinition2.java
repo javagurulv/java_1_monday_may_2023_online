e9a197c3238959708e093bec39fdc6cdbed8c471
javagurulv <javagurulv@gmail.com>
2023-07-27 11:36:32 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_1/solutions/ArrayDefinition2.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_1.solutions;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
+
+}
