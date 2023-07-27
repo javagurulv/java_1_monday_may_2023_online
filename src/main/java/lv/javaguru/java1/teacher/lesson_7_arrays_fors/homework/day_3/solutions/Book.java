deeb3f39b1b6e86bd67bd35007a1c3133ce82494
javagurulv <javagurulv@gmail.com>
2023-07-27 12:06:35 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_3/solutions/Book.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_3.solutions;
+
+class Book {
+
+    private String title;
+    private String author;
+    private int yearOfIssue;
+
+
+}
