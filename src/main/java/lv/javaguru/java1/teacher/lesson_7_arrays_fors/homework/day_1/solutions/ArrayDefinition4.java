e693baa79dfa0d02748255da2efb6888da40b43c
javagurulv <javagurulv@gmail.com>
2023-07-27 11:38:40 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_1/solutions/ArrayDefinition4.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_1.solutions;
+
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+    }
+
+}
