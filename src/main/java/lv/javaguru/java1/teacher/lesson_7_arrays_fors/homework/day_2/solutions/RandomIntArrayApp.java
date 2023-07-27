dbb042348fe2092cb4708ad1c8ec892cf3ba70b9
javagurulv <javagurulv@gmail.com>
2023-07-27 11:49:05 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_2/solutions/RandomIntArrayApp.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_2.solutions;
+
+class RandomIntArrayApp {
+
+    public static void main(String[] args) {
+        RandomIntArray randomIntArray = new RandomIntArray();
+        int[] array = randomIntArray.createAndFillArray();
+        randomIntArray.printArray(array);
+    }
+
+}
