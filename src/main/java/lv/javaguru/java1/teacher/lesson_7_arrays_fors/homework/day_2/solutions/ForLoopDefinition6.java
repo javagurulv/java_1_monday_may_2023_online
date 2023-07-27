9df7a8fc6932cb92ccc914ea42c406d341676afa
javagurulv <javagurulv@gmail.com>
2023-07-27 12:03:29 +0300

    Lesson 7 solutions

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_arrays_fors/homework/day_2/solutions/ForLoopDefinition6.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.teacher.lesson_7_arrays_fors.homework.day_2.solutions;
+
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
