420c901b5e02c88c62cea1676b793e596ad228e9
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-07-29 21:37:17 +0100

    lesson 7 homework day 3 updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_7/homework/day_3/BookLibraryTest.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_7/homework/day_3/BookLibraryTest.java
@@ -1,80 +1,72 @@
 package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_7.homework.day_3;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 class BookLibraryTest {
     public static void main(String[] args) {
-        List<Book> allBooks = new ArrayList<>();
-        BookLibrary bookLibrary = new BookLibrary();
-        bookLibrary.addBook(allBooks,"The Little Prince", "Antoine de Saint-Exupery", 1943, 50);
-        bookLibrary.addBook(allBooks,"The Alchemist", "Paulo Coelho", 1988, 100);
-        bookLibrary.addBook(allBooks,"Reach dad poor dad", "Robert Kiyosaki", 1998, 175);
+
         BookLibraryTest test = new  BookLibraryTest();
-        test.testFindBookByTitle(allBooks);
-        test.testFindBookByAuthor(allBooks);
-        test.testSumPage(allBooks);
-        test.testCountBooks(allBooks);
-        test.testRemoveBook(allBooks);
+        test.testFoundOneBookByTitle();
+        test.testFoundOneBookByAuthor();
+        test.testSumPagesByAuthor();
+        test.testCountBooks();
+        test.testRemoveBook();
     }
 
-    public void testFindBookByTitle(List<Book> allBooks) {
+    public void testFoundOneBookByTitle() {
         BookLibrary bookLibrary = new BookLibrary();
-        Book book1 = allBooks.get(0);
-        List<Book> result = bookLibrary.findBookByTitle(allBooks, "The Little Prince");
-        Book book2 = result.get(0);
-        if (book2.getTitle().equals(book1.getTitle()))
-        {
-            System.out.println(": TEST OK!");
-        } else {
-            System.out.println(": TEST FAIL!");
-        }
+        Book book = new Book("The Little Prince", "Antoine de Saint-Exupery", 1943, 50);
+        bookLibrary.addBook(book);
+        List<Book> foundBooks = bookLibrary.findBooksByTitle("The Little Prince");
+        checkResult(foundBooks.size(), 1, "testFoundOneBookByTitle");
     }
 
-    public void testFindBookByAuthor(List<Book> allBooks) {
+    public void testFoundOneBookByAuthor() {
         BookLibrary bookLibrary = new BookLibrary();
-        Book book1 = allBooks.get(0);
-        List<Book> result = bookLibrary.findBookByAuthor(allBooks,"Antoine de Saint-Exupery");
-        Book book2 = result.get(0);
-        if (book2.getAuthor().equals(book1.getAuthor())) {
-            System.out.println(": TEST OK!");
-            } else {
-            System.out.println(": TEST FAIL!");
-            }
-        }
-
-    public void testSumPage(List<Book> allBooks) {
-        int pageSum = 50;
-        BookLibrary bookLibrary = new BookLibrary();
-        int result = bookLibrary.sumPage(allBooks,"Antoine de Saint-Exupery");
-        System.out.println("RESULT: " + result);
-        if (result == pageSum) {
-            System.out.println(": TEST OK!");
-        } else {
-            System.out.println(": TEST FAIL!");
-        }
-    }
-    public void testCountBooks(List<Book> allBooks) {
-        int BooksCount = 3;
-        BookLibrary bookLibrary = new BookLibrary();
-        int result = bookLibrary.countBooks(allBooks);
-        System.out.println("RESULT: " + result);
-        if (result == BooksCount) {
-            System.out.println(": TEST OK!");
-        } else {
-            System.out.println(": TEST FAIL!");
-        }
+        Book book = new Book("The Little Prince", "Antoine de Saint-Exupery", 1943, 50);
+        bookLibrary.addBook(book);
+        List<Book> foundBooks = bookLibrary.findBooksByAuthor("Antoine de Saint-Exupery");
+        checkResult(foundBooks.size(), 1, "testFoundOneBookByAuthor");
     }
 
-    public void testRemoveBook(List<Book> allBooks) {
+
+    public void testSumPagesByAuthor() {
         BookLibrary bookLibrary = new BookLibrary();
-        bookLibrary.removeBook(allBooks, "The Alchemist", "Paulo Coelho", 1988);
-        List<Book> result = bookLibrary.findBookByTitle(allBooks, "The Alchemist");
-        if (result.size() == 0)
-        {
-            System.out.println(": TEST OK!");
+        Book book = new Book("The Little Prince", "Antoine de Saint-Exupery", 1943, 50);
+        bookLibrary.addBook(book);
+        int pageCount = bookLibrary.sumPagesByAuthor("Antoine de Saint-Exupery");
+        checkResult(pageCount, 50, "testSumPagesByAuthor");
+    }
+
+
+    public void testCountBooks() {
+        BookLibrary bookLibrary = new BookLibrary();
+        Book book1 = new Book("The Little Prince", "Antoine de Saint-Exupery", 1943, 50);
+        bookLibrary.addBook(book1);
+        Book book2 = new Book("The Alchemist", "Paulo Coelho", 1988, 100);
+        bookLibrary.addBook(book2);
+        Book book3 = new Book("Reach dad poor dad", "Robert Kiyosaki", 1998, 175);
+        bookLibrary.addBook(book3);
+        int bookCount = bookLibrary.getBookCount();
+        checkResult(bookCount, 3, "testCountBooks");
+
+    }
+
+    public void testRemoveBook() {
+        BookLibrary bookLibrary = new BookLibrary();
+        bookLibrary.removeBook( "The Alchemist", "Paulo Coelho", 1988);
+        List<Book> foundBooks = bookLibrary.findBooksByTitle( "The Alchemist");
+        checkResult(foundBooks.size(), 0, "testRemoveBook");
+    }
+
+    private void checkResult(int realResult,
+                             int expectedResult,
+                             String testScenarioName) {
+        if (realResult == expectedResult) {
+            System.out.println(testScenarioName + ": TEST OK!");
         } else {
-            System.out.println(": TEST FAIL!");
+            System.out.println(testScenarioName + ": TEST FAIL!");
         }
     }
 
