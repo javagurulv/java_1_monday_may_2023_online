7a81cd6b6511ca6ea8d238b002deb040371c3188
javagurulv <javagurulv@gmail.com>
2023-07-30 09:24:27 +0300

    Lesson 9

--- src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_6/homework/day_6/SalaryTaxCalculatorTest.java
+++ src/main/java/lv/javaguru/java1/student_ainars_belinskis/lesson_6/homework/day_6/SalaryTaxCalculatorTest.java
@@ -43,4 +43,18 @@ class SalaryTaxCalculatorTest {
         }
     }
 
+    // вот это более правильный метод сравнения чисел с запятой
+    // это связано с представлением вещественных чисел в памяти компьютера
+    // https://javarush.com/groups/posts/2136-ustroystvo-vejshestvennihkh-chisel
+    private void checkResultWithThreshold(double realResult,
+                                          double expectedResult,
+                                          String testScenarioName) {
+        double threshold = 0.000001d;
+        if (Math.abs(realResult - expectedResult) < threshold) {
+            System.out.println(testScenarioName + ": TEST OK!");
+        } else {
+            System.out.println(testScenarioName + ": TEST FAIL! realResult = " + realResult + " expectedResult = " + expectedResult);
+        }
+    }
+
 }
