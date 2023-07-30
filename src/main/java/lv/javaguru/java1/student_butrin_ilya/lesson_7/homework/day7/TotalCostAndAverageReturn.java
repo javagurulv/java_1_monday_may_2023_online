3e955f575cc8adf92ab24bfae66cde985023687e
javagurulv <javagurulv@gmail.com>
2023-07-30 10:47:05 +0300

    Lesson 9

--- src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_7/homework/day7/TotalCostAndAverageReturn.java
+++ src/main/java/lv/javaguru/java1/student_butrin_ilya/lesson_7/homework/day7/TotalCostAndAverageReturn.java
@@ -1,6 +1,9 @@
 package lv.javaguru.java1.student_butrin_ilya.lesson_7.homework.day7;
 
 public class TotalCostAndAverageReturn {
+
+    // в названии метода нет глагола показывающего какое действие
+    // совершает этот метод
     public double totalCoast (Stock[] stock) {
         double totalCoast = 0;
         for (Stock a : stock) {
@@ -8,6 +11,9 @@ public class TotalCostAndAverageReturn {
         }
         return  totalCoast;
     }
+
+    // в названии метода нет глагола показывающего какое действие
+    // совершает этот метод
     public double averageReturn(Stock[] stock) {
         double averagereturn = 0;
         for (Stock a : stock) {
