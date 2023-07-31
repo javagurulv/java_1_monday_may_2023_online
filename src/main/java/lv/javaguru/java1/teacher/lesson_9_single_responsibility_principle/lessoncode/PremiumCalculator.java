a03324d8496be7ffda8e225a7f41aedd246ebfc2
javagurulv <javagurulv@gmail.com>
2023-07-31 22:01:26 +0300

    Lesson 9

--- src/main/java/lv/javaguru/java1/teacher/lesson_9_single_responsibility_principle/lessoncode/PremiumCalculator.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_9_single_responsibility_principle/lessoncode/PremiumCalculator.java
@@ -5,28 +5,6 @@ import java.util.List;
 
 public class PremiumCalculator {
 
-    private BigDecimal calculatePremiumFire(InsuredObject object) {
-        // 1.1. calculate SUM_INSURED_FIRE -
-        // sum of all subObject sums what insured from FIRE
-        BigDecimal sumInsuredFire = BigDecimal.ZERO;
-        for (InsuredSubObject subObject : object.getSubObjects()) {
-            List<RiskType> subObjectRisks = subObject.getRisks();
-            if (subObjectRisks.contains(RiskType.FIRE)) {
-                sumInsuredFire = sumInsuredFire.add(subObject.getSum());
-            }
-        }
-
-        // 1.2. choice COEFFICIENT_FIRE (0.014 or 0.024)
-        BigDecimal coefficientFire = new BigDecimal("0.014");
-        BigDecimal oneHundred = new BigDecimal("100");
-        if (sumInsuredFire.compareTo(oneHundred) > 0) {
-            coefficientFire = new BigDecimal("0.024");
-        }
-
-        // 1.3. PREMIUM_FIRE = SUM_INSURED_FIRE * COEFFICIENT_FIRE
-        return sumInsuredFire.multiply(coefficientFire);
-    }
-
     public BigDecimal calculate(Policy policy) {
         BigDecimal premium = BigDecimal.ZERO;
 
@@ -53,5 +31,28 @@ public class PremiumCalculator {
 
         return premium;
     }
+
+    private BigDecimal calculatePremiumFire(InsuredObject object) {
+        // 1.1. calculate SUM_INSURED_FIRE -
+        // sum of all subObject sums what insured from FIRE
+        BigDecimal sumInsuredFire = BigDecimal.ZERO;
+        for (InsuredSubObject subObject : object.getSubObjects()) {
+            List<RiskType> subObjectRisks = subObject.getRisks();
+            if (subObjectRisks.contains(RiskType.FIRE)) {
+                sumInsuredFire = sumInsuredFire.add(subObject.getSum());
+            }
+        }
+
+        // 1.2. choice COEFFICIENT_FIRE (0.014 or 0.024)
+        BigDecimal coefficientFire = new BigDecimal("0.014");
+        BigDecimal oneHundred = new BigDecimal("100");
+        if (sumInsuredFire.compareTo(oneHundred) > 0) {
+            coefficientFire = new BigDecimal("0.024");
+        }
+
+        // 1.3. PREMIUM_FIRE = SUM_INSURED_FIRE * COEFFICIENT_FIRE
+        return sumInsuredFire.multiply(coefficientFire);
+    }
+
 
 }
