8be97352240290d062a0a5eff273195e85a5fb9e
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-08-03 00:09:56 +0100

    lesson 8 homework day 3

---
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_8/day_3/DuplicatesInList.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_8.day_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesInList {
+
+    public static void main(String[] args) {
+        List<Integer> integerList1 = new ArrayList<>();
+        integerList1.add(7);
+        integerList1.add(7);
+        integerList1.add(7);
+        integerList1.add(7);
+
+     System.out.println(integerList1);
+    }
+}
