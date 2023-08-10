6e99f9d231f152b0b661254c37b2090f3be7381b
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-08-10 23:50:25 +0100

    lesson 9 lessoncode premium calculator test updated

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_9/lessoncode/PremiumCalculatorTest.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_9/lessoncode/PremiumCalculatorTest.java
@@ -9,191 +9,154 @@ class PremiumCalculatorTest {
     public static void main(String[] args) {
 
         PremiumCalculatorTest test = new PremiumCalculatorTest();
-        test.testCalculatePremiumOneSubObjectOneObjectV1();
-        test.testCalculatePremiumTwoSubObjectsOneObjectV2();
-        test.testCalculatePremiumFourSubObjectsTwoObjectsV3();
-        test.testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseOneV4();
-        test.testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseTwoV5();
+        test.testCalculatePremiumOneObjectOneSubObjectFireSumInjuredUnder100();
+        test.testCalculatePremiumOneObjectOneSubObjectFireSumInjured100();
+        test.testCalculatePremiumOneObjectOneSubObjectFireSumInjuredOver100();
+        test.testCalculatePremiumOneObjectTwoSubObjectsFireSumInjuredOver100();
+        test.testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredUnder15();
+        test.testCalculatePremiumOneObjectOneSubObjectTheftSumInjured15();
+        test.testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredOver15();
+        test.testCalculatePremiumOneObjectTwoSubObjectsTheftSumInjuredOver15();
+        test.testCalculatePremiumTwoObjectsTwoSubObjectsFireTheft();
     }
 
-        public void testCalculatePremiumOneSubObjectOneObjectV1() {
+    public void testCalculatePremiumOneObjectOneSubObjectFireSumInjuredUnder100() {
         PremiumCalculator premiumCalculator = new PremiumCalculator();
-            InsuredSubObject tv = new InsuredSubObject(
-                    "TV",
-                    new BigDecimal("1000.50"),
-                    List.of(RiskType.FIRE)
-            );
+        InsuredSubObject tv = new InsuredSubObject("TV", new BigDecimal("99.50"), List.of(RiskType.FIRE));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(tv);
 
-            List<InsuredSubObject> subObjects = new ArrayList();
-            subObjects.add(tv);
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
 
-            InsuredObject object1 = new InsuredObject(
-                    "House1",
-                    subObjects
-            );
-
-            List<InsuredObject> objects = new ArrayList();
-            objects.add(object1);
-
-
-            BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5",
-                    PolicyStatus.REGISTERED,
-                    objects));
-            checkResult(premium, new BigDecimal( "24.01"),"testCalculatePremiumOneSubObjectOneObjectV1");
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "1.39"),"testCalculatePremiumOneObjectOneSubObjectFireSumInjuredUnder100");
         }
 
-    public void testCalculatePremiumTwoSubObjectsOneObjectV2() {
+    public void testCalculatePremiumOneObjectOneSubObjectFireSumInjured100() {
         PremiumCalculator premiumCalculator = new PremiumCalculator();
-        InsuredSubObject tv = new InsuredSubObject(
-                "TV",
-                new BigDecimal("1000.50"),
-                List.of(RiskType.FIRE)
-        );
+        InsuredSubObject tv = new InsuredSubObject("TV", new BigDecimal("100.00"), List.of(RiskType.FIRE));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(tv);
 
-        InsuredSubObject fridge = new InsuredSubObject(
-                "Fridge",
-                new BigDecimal("500.00"),
-                List.of(RiskType.FIRE, RiskType.THEFT)
-        );
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
 
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "1.40"),"testCalculatePremiumOneObjectOneSubObjectFireSumInjured100");
+    }
+
+    public void testCalculatePremiumOneObjectOneSubObjectFireSumInjuredOver100() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject tv = new InsuredSubObject("TV", new BigDecimal("1000.50"), List.of(RiskType.FIRE));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(tv);
+
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
+
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "24.01"),"testCalculatePremiumOneObjectOneSubObjectFireSumInjuredOver100");
+    }
+
+    public void testCalculatePremiumOneObjectTwoSubObjectsFireSumInjuredOver100() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject tv = new InsuredSubObject("TV", new BigDecimal("1000.50"), List.of(RiskType.FIRE));
+        InsuredSubObject fridge = new InsuredSubObject("Fridge", new BigDecimal("500.00"), List.of(RiskType.FIRE));
         List<InsuredSubObject> subObjects = new ArrayList();
         subObjects.add(tv);
         subObjects.add(fridge);
 
-        InsuredObject object1 = new InsuredObject(
-                "House1",
-                subObjects
-        );
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
 
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "36.01"),"testCalculatePremiumOneObjectTwoSubObjectsFireSumInjuredOver100");
+    }
+
+
+    public void testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredUnder15() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject wallClock = new InsuredSubObject("Wall Clock", new BigDecimal("8.00"), List.of(RiskType.THEFT));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(wallClock);
+
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
         List<InsuredObject> objects = new ArrayList();
         objects.add(object1);
 
 
-        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5",
-                PolicyStatus.REGISTERED,
-                objects));
-        checkResult(premium, new BigDecimal( "61.01"),"testCalculatePremiumTwoSubObjectsOneObjectV2");
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "0.88"),"testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredUnder15");
     }
 
-    public void testCalculatePremiumFourSubObjectsTwoObjectsV3() {
+    public void testCalculatePremiumOneObjectOneSubObjectTheftSumInjured15() {
         PremiumCalculator premiumCalculator = new PremiumCalculator();
-        InsuredSubObject tv = new InsuredSubObject(
-                "TV",
-                new BigDecimal("1000.50"),
-                List.of(RiskType.FIRE)
-        );
+        InsuredSubObject wallClock = new InsuredSubObject("Wall Clock", new BigDecimal("15.00"), List.of(RiskType.THEFT));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(wallClock);
 
-        InsuredSubObject fridge = new InsuredSubObject(
-                "Fridge",
-                new BigDecimal("500.00"),
-                List.of(RiskType.FIRE, RiskType.THEFT)
-        );
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
 
+
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "1.65"),"testCalculatePremiumOneObjectOneSubObjectTheftSumInjured15");
+    }
+
+    public void testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredOver15() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject blender = new InsuredSubObject("Blender", new BigDecimal("50.51"), List.of(RiskType.THEFT));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(blender);
+
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
+
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "2.53"),"testCalculatePremiumOneObjectOneSubObjectTheftSumInjuredOver15");
+    }
+
+    public void testCalculatePremiumOneObjectTwoSubObjectsTheftSumInjuredOver15() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject wallClock = new InsuredSubObject("Wall Clock", new BigDecimal("35.00"), List.of(RiskType.THEFT));
+        InsuredSubObject blender = new InsuredSubObject("Blender", new BigDecimal("50.51"), List.of(RiskType.THEFT));
+        List<InsuredSubObject> subObjects = new ArrayList();
+        subObjects.add(wallClock);
+        subObjects.add(blender);
+
+        InsuredObject object1 = new InsuredObject("House1", subObjects);
+        List<InsuredObject> objects = new ArrayList();
+        objects.add(object1);
+
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "4.28"),"testCalculatePremiumOneObjectTwoSubObjectsTheftSumInjuredOver15");
+    }
+
+    public void testCalculatePremiumTwoObjectsTwoSubObjectsFireTheft() {
+        PremiumCalculator premiumCalculator = new PremiumCalculator();
+        InsuredSubObject tv = new InsuredSubObject("TV", new BigDecimal("1000.50"), List.of(RiskType.FIRE));
         List<InsuredSubObject> subObjects1 = new ArrayList();
         subObjects1.add(tv);
-        subObjects1.add(fridge);
-
-        InsuredSubObject computer = new InsuredSubObject(
-                "Computer",
-                new BigDecimal("2000.00"),
-                List.of(RiskType.FIRE, RiskType.THEFT)
-        );
-
-        InsuredSubObject vacuumCleaner = new InsuredSubObject(
-                "Vacuum Cleaner",
-                new BigDecimal("700.00"),
-                List.of(RiskType.FIRE)
-        );
-
+        InsuredSubObject computer = new InsuredSubObject("Computer", new BigDecimal("2000.00"), List.of(RiskType.THEFT));
         List<InsuredSubObject> subObjects2 = new ArrayList();
         subObjects2.add(computer);
-        subObjects2.add(vacuumCleaner);
 
-        InsuredObject object1 = new InsuredObject(
-                "House1",
-                subObjects1
-        );
-
-        InsuredObject object2 = new InsuredObject(
-                "House2",
-                subObjects2
-        );
+        InsuredObject object1 = new InsuredObject("House1", subObjects1);
+        InsuredObject object2 = new InsuredObject("House2", subObjects2);
 
         List<InsuredObject> objects = new ArrayList();
         objects.add(object1);
         objects.add(object2);
 
-
-        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5",
-                PolicyStatus.REGISTERED,
-                objects));
-        checkResult(premium, new BigDecimal( "225.81"),"testCalculatePremiumFourSubObjectsTwoObjectsV3");
-    }
-
-    public void testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseOneV4() {
-        PremiumCalculator premiumCalculator = new PremiumCalculator();
-        InsuredSubObject blender = new InsuredSubObject(
-                "Blender",
-                new BigDecimal("100.00"),
-                List.of(RiskType.FIRE)
-        );
-
-        InsuredSubObject wallClock = new InsuredSubObject(
-                "Wall Clock",
-                new BigDecimal("8.00"),
-                List.of(RiskType.THEFT)
-        );
-
-        List<InsuredSubObject> subObjects = new ArrayList();
-        subObjects.add(blender);
-        subObjects.add(wallClock);
-
-        InsuredObject object1 = new InsuredObject(
-                "House1",
-                subObjects
-        );
-
-        List<InsuredObject> objects = new ArrayList();
-        objects.add(object1);
-
-
-        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5",
-                PolicyStatus.REGISTERED,
-                objects));
-        checkResult(premium, new BigDecimal( "2.28"),"testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseOneV4");
-    }
-
-    public void testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseTwoV5() {
-        PremiumCalculator premiumCalculator = new PremiumCalculator();
-        InsuredSubObject washingMachine = new InsuredSubObject(
-                "Washing Machine",
-                new BigDecimal("500.00"),
-                List.of(RiskType.FIRE)
-        );
-
-        InsuredSubObject blender = new InsuredSubObject(
-                "Blender",
-                new BigDecimal("102.51"),
-                List.of(RiskType.THEFT)
-        );
-
-        List<InsuredSubObject> subObjects = new ArrayList();
-        subObjects.add(washingMachine);
-        subObjects.add(blender);
-
-        InsuredObject object1 = new InsuredObject(
-                "House1",
-                subObjects
-        );
-
-        List<InsuredObject> objects = new ArrayList();
-        objects.add(object1);
-
-
-        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5",
-                PolicyStatus.REGISTERED,
-                objects));
-        checkResult(premium, new BigDecimal( "17.13"),"testCalculatePremiumTwoSubObjectsOneObjectAcceptanceCriteriaCaseTwoV5");
+        BigDecimal premium = premiumCalculator.calculate(new Policy("LV20-02-100000-5", PolicyStatus.REGISTERED, objects));
+        checkResult(premium, new BigDecimal( "124.01"),"testCalculatePremiumTwoObjectsTwoSubObjectsFireTheft");
     }
 
 
