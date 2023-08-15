0cd249a1bc637ceca927ce5215fa4e58d94c5b10
Alex-Likurg <likurg68@gmail.com>
2023-08-15 21:54:08 +0300

    lesson8
    Task1

---
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_8/day_1/ListLinkDataTypesObject.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_alexey_kosmachev.lesson_8.day_1;
+
+class ListLinkDataTypesObject {
+
+    String text;
+    int number;
+
+    public ListLinkDataTypesObject(String text, int number) {
+        this.text = text;
+        this.number = number;
+    }
+    public String getText(){return text; }
+    public int getNumber() {return number;}
+}
