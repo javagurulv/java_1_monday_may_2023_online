057f6eef4b258b2554dd768effcfa9c146587bf7
TG_B <timur.geldiev@gmail.com>
2023-08-21 11:28:30 +0300

    Lesson 11 homework day4

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_11/homework/day_4/CelsiusConverter.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_11.homework.day_4;
+
+public class CelsiusConverter implements TemperatureConverter{
+
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature;
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_11/homework/day_4/TemperatureConverter.java
@@ -0,0 +1,6 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_11.homework.day_4;
+
+public interface TemperatureConverter {
+    double convert(double celsiusTemperature);
+
+}
