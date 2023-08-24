ac752715e04e1459070641936f3134921dc8e547
TatjanaGrigorcaka <tatjana.grigorcaka@gmail.com>
2023-08-24 23:12:59 +0100

    Premium Calculator updated rounding function

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_11/lessoncode/v2/PremiumCalculatorV2Impl.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_11/lessoncode/v2/PremiumCalculatorV2Impl.java
@@ -16,9 +16,7 @@ public class PremiumCalculatorV2Impl implements PremiumCalculator {
         BigDecimal premium = BigDecimal.ZERO;
         for (InsuredObject object : policy.getObjects()) {
             BigDecimal insuredObjectPremium = calculateInsuredObjectPremium(object);
-            premium = premium.add(insuredObjectPremium);
-            premium = new
-                    BigDecimal(String.valueOf(premium)).setScale(2, RoundingMode.HALF_UP);
+            premium = premium.add(insuredObjectPremium).setScale(2, RoundingMode.HALF_DOWN);
         }
         return premium;
     }
