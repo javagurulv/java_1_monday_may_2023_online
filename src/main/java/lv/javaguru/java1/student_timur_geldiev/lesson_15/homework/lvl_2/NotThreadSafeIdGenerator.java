2db3918c127482e70e94dc60d76824681ff88e68
TG_B <timur.geldiev@gmail.com>
2023-09-15 14:34:17 +0300

    Lesson 15 homework LVL2

---
+++ src/main/java/lv/javaguru/java1/student_timur_geldiev/lesson_15/homework/lvl_2/NotThreadSafeIdGenerator.java
@@ -0,0 +1,50 @@
+package lv.javaguru.java1.student_timur_geldiev.lesson_15.homework.lvl_2;
+
+class NotThreadSafeIdGenerator {
+
+    private int id;
+
+    public int nextId() {
+        return id++;
+    }
+
+    public static void main(String[] args) {
+
+        NotThreadSafeIdGenerator idGenerator = new NotThreadSafeIdGenerator();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int id1 = idGenerator.nextId();
+                System.out.println("Thread 1: " + id1);
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int id2 = idGenerator.nextId();
+                System.out.println("Thread 2: " + id2);
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int id3 = idGenerator.nextId();
+                System.out.println("Thread 3: " + id3);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+
+    }
+}
