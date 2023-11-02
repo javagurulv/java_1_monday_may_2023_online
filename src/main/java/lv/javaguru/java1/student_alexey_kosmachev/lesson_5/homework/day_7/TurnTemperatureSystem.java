5d6c3b8f1bbb5bd7d2e4244fc919faf75212cd13
Alex-Likurg <likurg68@gmail.com>
2023-11-02 19:00:11 +0200

    lesson 5
    Task corrections

--- src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/day_7/TurnTemperatureSystem.java
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_5/homework/day_7/TurnTemperatureSystem.java
@@ -2,12 +2,10 @@ package lv.javaguru.java1.student_alexey_kosmachev.lesson_5.homework.day_7;
 
 class TurnTemperatureSystem {
     public double degreeCelius(double degree1) {
-        double inFarengate = (degree1 * 9/5) + 32;
-        {return inFarengate;}
+        return (degree1 * 9/5) + 32;
     }
 
     public double degreeFarengate(double degree2) {
-        double inCelsius = (degree2 - 32) * 5 / 9;
-        {return inCelsius;}
+        return (degree2 - 32) * 5 / 9;
     }
 }
