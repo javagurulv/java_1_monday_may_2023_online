dea14487d1d6787dd680ed477d3527eddc503f68
Asus <tehnokontrolj@inbox.lv>
2023-11-23 00:00:29 +0200

    TaxCalculator.java; TaxCalculatorImpl.java; TaxCalculatorImplTest.java

---
+++ src/main/java/lv/javaguru/java1/student_aleksandr_kalninjsh/lesson_10/TaxCalculator.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_aleksandr_kalninjsh.lesson_10;
+
+import java.math.BigDecimal;
+
+public interface TaxCalculator {
+
+    BigDecimal calculateTax(BigDecimal income);
+
+
+}
