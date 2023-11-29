85f2b9cfdf97460a3511b321aaadffc21e1cfaff
Alex-Likurg <likurg68@gmail.com>
2023-11-29 18:25:12 +0200

    lesson 7
    day 2. corrections

--- src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_2/ForLoopDefinition4.java
+++ src/main/java/lv/javaguru/java1/student_alexey_kosmachev/lesson_7/homework/day_2/ForLoopDefinition4.java
@@ -4,7 +4,7 @@ package lv.javaguru.java1.student_alexey_kosmachev.lesson_7.homework.day_2;
 class ForLoopDefinition4 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) { // необходимы плюсы. При минусах процесс бесконечный, нет нижней границы
+        for (int i = 0; i < 10; i++) { // Необходимы плюсы. При минусах процесс бесконечный, нет нижней границы
             System.out.println(i);
         }
     }
