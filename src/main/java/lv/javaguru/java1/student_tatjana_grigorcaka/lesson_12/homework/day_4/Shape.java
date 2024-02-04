8bfeecc763a594321ffb8e7075b6e032a63c0dbc
Asus <tehnokontrolj@inbox.lv>
2024-02-04 16:17:30 +0200

    Circle; Shape; ShapeUtilTest  BUG FIX

--- src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_12/homework/day_4/Shape.java
+++ src/main/java/lv/javaguru/java1/student_tatjana_grigorcaka/lesson_12/homework/day_4/Shape.java
@@ -1,14 +1,19 @@
 package lv.javaguru.java1.student_tatjana_grigorcaka.lesson_12.homework.day_4;
 
 public abstract class Shape {
+
     private String title;
+
 
     Shape(String title) {
         this.title = title;
+
     }
 
     public abstract double calculateArea();
-
     public abstract double calculatePerimeter();
+    public String getTitle() {
+        return title;
+    }
 
 }
